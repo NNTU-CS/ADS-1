b4176765d0fea71a47b4fbbdbe95f13540e2658a
RDaria <78913752+RDaria@users.noreply.github.com>
2021-03-21 13:42:21 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,5 +2,23 @@
 
 int cbinsearch(int *arr, int size, int value) {
   //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+  int right=0, left=size-1, sum=0;//левая и правая границы массива
+  while (right<left) {// пока не сошлись границы массива
+    int middle=(right+left)/2;// индекс среднего элемента
+// сужаем массив
+    if (arr[middle]<value)
+      right=middle+1;
+    else
+      left=middle;
+  }
+//подсчёт количества искомого числа
+  if (arr[right]==value) {
+    while(arr[right]==value) {
+      sum++;
+      right++;
+    }
+  }
+  if (sum!=0)
+    return sum;
+  else return 0; // если ничего не найдено
 }
