0048a136307774efef23dbf4663cee11ab1fb0f2
Alexander-Zaplatin <79161134+Alexander-Zaplatin@users.noreply.github.com>
2021-03-25 00:02:37 +0300

    Заплатин Александр

--- src/alg.cpp
+++ src/alg.cpp
@@ -8,20 +8,21 @@ int e1 = 0;
 int m = 0;
 int mid;
 while ((l <= r) && (e != value)) {
- mid = (l + r) / 2;
-	if (arr[mid] == value) e = mid;
-	if (arr[mid] > value) r = mid - 1;
- else l = mid + 1;
+mid = (l + r) / 2;
+ if (arr[mid] == value) e = mid;
+ if (arr[mid] > value) r = mid - 1;
+else 
+ l = mid + 1;
 }
 e1 = e;
 while (arr[e1] == value) {
- ++m;
- ++e1;
+++m;
+++e1;
 }
 e1 = e-1;
 while (arr[e1] == value) {
- ++m;
- --e1;
+++m;
+--e1;
 }
 cout << m;
 return 0;
