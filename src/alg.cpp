cfaa3ac63ad7c4d8d1c177b9809ca86b9359bf2c
ogarkovavalentina <79150763+ogarkovavalentina@users.noreply.github.com>
2021-03-25 00:18:34 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,29 +1,25 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-    int right, left = 0, sum = 0, midlef;
-    right = size - 1;
+    int left = 0, right = size - 1, count = 0;
     while (left < right) {
-        midlef = (left + (right - left)) / 2;
-        midlef = (right - left) / 2 + left;
-        if (arr[midlef] == value) {
-            while (arr[midlef - 1] == value) {
-                sum++;
-                midlef--;
-            }
-            midlef += sum;
-            while (arr[midlef] == value) {
-                midlef++;
-                sum++;
-            }
-            return sum;
-        }
-        if (arr[midlef] > value) {
-            right = midl;
-        } else {
-            left = midlef + 1;
-        }
+    int m = (left + right) / 2;
+    if (arr[m] < value)
+    left = m + 1;
+    else
+    right = m;
     }
+
+    if (arr[left] == value) {
+    while (arr[left] == value) {
+    count++;
+    left++;
+    }
+    }
+
+    if (count)
+    return count;
+    else
     return 0; // если ничего не найдено
 }
 
