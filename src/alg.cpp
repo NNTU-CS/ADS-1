7700933c78a448d1d5710c49856f942073b20d17
Alexander-Zaplatin <79161134+Alexander-Zaplatin@users.noreply.github.com>
2021-03-24 23:47:57 +0300

    Заплатин Александр

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,28 +1,28 @@
 // Copyright 2021 NNTU-CS
 
-int cbinsearch(int arr, int size, int value) {
+int cbinsearch(int *arr, int size, int value) {
 int l = 0;
 int r = size - 1;
+int e = 0;
+int e1 = 0;
+int m = 0;
 int mid;
-int midL;
-int midR;
-int eL;
-int eR;
-int m;
-mid = (l + r) / 2;
-while ((l <= mid) && (eL != value)) {
- midL = (l + mid) / 2;
- if (arr[midL] == value) eL = midL;
- if (arr[midL] > value) mid = midL - 1;
- else l = midL + 1;
+while ((l <= r) && (e != value)) {
+ mid = (l + r) / 2;
+	if (arr[mid] == value) e = mid;
+	if (arr[mid] > value) r = mid - 1;
+ else l = mid + 1;
 }
-mid = (l + r) / 2;
-while ((mid <= r) && (eR != value)) {
- midR = (r + mid) / 2;
- if (arr[midR] == value) eR = midR;
- if (arr[midR] > value) mid = r - 1;
- else mid = midR + 1;
+e1 = e;
+while (arr[e1] == value) {
+ ++m;
+ ++e1;
 }
-m = (midR - midL) + 1;
-return m;
+e1 = e-1;
+while (arr[e1] == value) {
+ ++m;
+ --e1;
+}
+cout << m;
+return 0;
 }
