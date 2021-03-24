f6bcc2c136b9ad49b49e69de1f70c22b2ebe472e
Alexander-Zaplatin <79161134+Alexander-Zaplatin@users.noreply.github.com>
2021-03-24 22:21:59 +0300

    Заплатин Александр

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,28 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+int l = 0;
+int r = size - 1;
+int mid;
+int midL;
+int midR;
+int eL;
+int eR;
+int m;
+mid = (l + r) / 2;
+while ((l <= mid) && (eL != value)) {
+ midL = (l + mid) / 2;
+ if (arr[midL] == value) eL = midL;
+ if (arr[midL] > value) mid = midL - 1;
+ else l = midL + 1;
+}
+mid = (l + r) / 2;
+while ((mid <= r) && (eR != value)) {
+ midR = (r + mid) / 2;
+ if (arr[midR] == value) eR = midR;
+ if (arr[midR] > value) mid = r - 1;
+ else mid = midR + 1;
+}
+m = (midR - midL) + 1;
+return m;
 }
