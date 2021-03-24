938138dc234dc5e3062a9156a901b7260f84215c
ogarkovavalentina <79150763+ogarkovavalentina@users.noreply.github.com>
2021-03-24 23:53:05 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,29 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+    int right, left = 0, sum = 0, midlef;
+    right = size - 1;
+    while (left < right) {
+        midlef = (left + (right - left)) / 2;
+        midlef = (right - left) / 2 + left;
+        if (arr[midlef] == value) {
+            while (arr[midlef - 1] == value) {
+                sum++;
+                midlef--;
+            }
+            midlef += sum;
+            while (arr[midlef] == value) {
+                midlef++;
+                sum++;
+            }
+            return sum;
+        }
+        if (arr[midlef] > value) {
+            right = midl;
+        } else {
+            left = midlef + 1;
+        }
+    }
+    return 0; // если ничего не найдено
 }
+
