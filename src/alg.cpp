07edf12e5cc6a248f8b7709c739b8e3b5e3e8339
BaRyan1 <76247481+BaRyan1@users.noreply.github.com>
2021-03-25 21:43:23 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,5 +2,11 @@
 
 int cbinsearch(int *arr, int size, int value) {
   //  поместить сюда реализацию алгоритма
+  if (*arr[mid] == value)
+{
+    for (int i = mid; i < size && *arr[i] == value; ++i)
+        kolvo++;
+    break;
+}
   return 0; // если ничего не найдено
 }
