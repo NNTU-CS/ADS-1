dcbf5062ed8ca7b084ff6072204330358452b7f7
koisoi <60881099+koisoi@users.noreply.github.com>
2021-04-03 17:48:08 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,31 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+  int first = 0, last = size - 1, count = 0, mid = (first + last) / 2;
+    while ((last != first+1))
+    {
+        if (value < arr[mid]) {
+            last = mid;
+            mid = (first + last) / 2;
+        }
+        else {
+            first = mid;
+            mid = (first + last) / 2;
+        }
+    }
+    if (arr[mid] == value)
+    {
+        count++;
+        int i = mid-1;
+        while ((i > -1) && (arr[i] == value)) {
+            count++;
+            i--;
+        }
+        i = mid + 1;
+        while ((i < size) && (arr[i] == value)) {
+            count++;
+            i++;
+        }
+    }
+    return count;
 }
