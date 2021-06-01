17788972b78d9a2aa08169d5a1d73859ac979bd6
rumkira20ist1 <78916886+rumkira20ist1@users.noreply.github.com>
2021-06-01 12:09:02 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,7 +1,6 @@
 // Copyright 2021 NNTU-CS
 int cbinsearch(int *arr, int size, int value) {
-   int i = 0, j = size - 1, countNum = 0;
-  
+ int i = 0, j = size - 1, countNum = 0;
     while (i < j) {
         int mid = (i + j) / 2;
         if (arr[mid] < value)
