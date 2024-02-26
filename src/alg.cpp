e62f1aeeb5213c9fd066b49b655619ee91bd0cf2
deniskr12 <kremnden@gmail.com>
2024-02-26 21:25:02 +0300

    ads

--- src/alg.cpp
+++ src/alg.cpp
@@ -10,13 +10,15 @@ bool checkPrime(uint64_t value) {
         }
         return true;
     }
-    else
+    else {
         return false;
+    }
+
 }
 uint64_t nPrime(uint64_t n) {
     uint64_t count = 0;
-    for (int i = 0;i<=10000;i++){
-        if (checkPrime(i)==true)
+    for (int i = 0;i <= 10000;i++) {
+        if (checkPrime(i) == true)
             count++;
         if (count == n)
             return i;
@@ -30,11 +32,10 @@ uint64_t nextPrime(uint64_t value) {
     }
     return 0;
 }
-uint64_t sumPrime(uint64_t hbound)
-{
+uint64_t sumPrime(uint64_t hbound){
     uint64_t sum = 0;
-    for (uint64_t i=2; i < hbound;i++)
-        if (checkPrime(i)==true)
+    for (uint64_t i = 2; i < hbound;i++)
+        if (checkPrime(i) == true)
             sum+=i;
     return sum;
 }
