cf620ce23211e07b6ab50ce366399ae47705406a
AndreyChetnev <159932012+AndreyChetnev@users.noreply.github.com>
2024-02-27 21:02:01 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,17 +4,44 @@
 
 
 bool checkPrime(uint64_t value) {
-// вставьте код функции
+  if (value <= 1) {
+    return false;
+  }
+  for (unit64_t i = 2; i < value; i++) {
+    if (value % i == 0){
+      return false;
+    }
+  }
+  return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-// вставьте код функции
+  if (checkPrime(n)){
+    unit64_t count = 0;
+    while (n != 1) {
+      if (checkPrime(n)) {
+        count++;
+      }
+      n--;
+    }
+    return count;
+  }
 }
 
 uint64_t nextPrime(uint64_t value) {
-// вставьте код функции
+  value +=1;
+  while (!checkPrime(value)) {
+    value++;
+  }
+  return value;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-// вставьте код функции
+  unit64_t sum = 0;
+  for (unit64_t = 2; i < hbound; i++) {
+    if (checkPrime(i)) {
+      sum +=i;
+    }
+  }
+  return sum;
 }
