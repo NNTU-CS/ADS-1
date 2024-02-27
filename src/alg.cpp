7f99f48680af5289e02a8147cac2c56b05f470c8
IanaVatagina <vatagina.iana@gmail.com>
2024-02-27 19:39:13 +0300

    Commit

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,48 +5,48 @@
 
 
 bool checkPrime(uint64_t value) {
-  bool flag=true;
-  float sqNum=sqrt(value);
-  for (int i=2; i<=sqNum; i++) {
-    if (value%i==0) {
-      flag=false;
+    bool flag = true;
+    float sqNum = sqrt(value);
+    for (int i = 2; i <= sqNum; i++) {
+        if (value % i == 0) {
+            flag = false;
+        }
     }
-  }
-  return flag;
+    return flag;
 }
 
 uint64_t nPrime(uint64_t n) {
-  int num=2;
-  float maxNum=pow(2, 64);
-  for (int i=1; i<n; i++) {
-    for (int j=num+1; j<maxNum; j++) {
-        if (checkPrime(j)) {
-            num=j;
-            break;
+    int num = 2;
+    float maxNum = pow(2, 64);
+    for (int i = 1; i < n; i++) {
+        for (int j =    + 1; j < maxNum; j++) {
+                if (checkPrime(j)) {
+                        num = j;
+                        break;
+                }
         }
     }
-  }
-  return num;
+    return num;
 }
 
 uint64_t nextPrime(uint64_t value) {
-  int result=0;
-  float maxNum=pow(2, 64);
-  for (int i=value+1; i<maxNum; i++) {
-    if (checkPrime(i)) {
-        result=i;
-        break;
+    int result = 0;
+    float maxNum = pow(2, 64);
+    for (int i = value + 1; i < maxNum; i++) {
+        if (checkPrime(i)) {
+                result = i;
+                break;
+        }
     }
-  }
-  return result;
+    return result;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-  int summa=0;
-  for (int i=2; i<hbound; i++) {
-    if (checkPrime(i)) {
-      summa+=i;
+    int summa = 0;
+    for (int i = 2; i < hbound; i++) {
+        if (checkPrime(i)) {
+            summa += i;
+        }
     }
-  }
-  return summa;
+    return summa;
 }
