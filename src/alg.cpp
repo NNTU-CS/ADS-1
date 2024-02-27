f64629381d42e167cd377f33a066d5b0d13afe0b
IanaVatagina <vatagina.iana@gmail.com>
2024-02-27 19:15:48 +0300

    Commit

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,40 +1,40 @@
-//Copyright[2022] <NNTU-CS>
-#include <cstdint>
+//Copyright2022 <NNTU-CS>
 #include "alg.h"
+#include <cstdint>
 #include <math.h>
 
 
 bool checkPrime(uint64_t value) {
-    bool flag = true;
-    float sqNum = sqrt(value);
-    for (int i = 2; i <= sqNum; i++) {
-        if (value % i == 0) {
-            flag = false;
+    bool flag=true;
+    float sqNum=sqrt(value);
+    for (int i=2; i<=sqNum; i++) {
+        if (value%i==0) {
+            flag=false;
         }
     }
     return flag;
 }
 
 uint64_t nPrime(uint64_t n) {
-    int num = 2;
-    float maxNum = pow(2, 64);
-    for (int i = 1; i < n; i++) {
-        for (int j = num + 1; j < maxNum; j++) {
+    int num=2;
+    float maxNum=pow(2, 64);
+    for (int i=1; i<n; i++) {
+        for (int j=num+1; j<maxNum; j++) {
             if (checkPrime(j)) {
-                num = j;
+                num=j;
                 break;
-			}
-		}
-	}
+            }
+        }
+    }
     return num;
 }
 
 uint64_t nextPrime(uint64_t value) {
-    int result = 0;
-    float maxNum = pow(2, 64);
-    for (int i = value + 1; i < maxNum; i++) {
+    int result=0;
+    float maxNum=pow(2, 64);
+    for (int i=value+1; i<maxNum; i++) {
         if (checkPrime(i)) {
-            result = i;
+            result=i;
             break;
         }
     }
@@ -42,10 +42,10 @@ uint64_t nextPrime(uint64_t value) {
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-    int summa = 0;
-    for (int i = 2; i < hbound; i++) {
+    int summa=0;
+    for (int i=2; i<hbound; i++) {
         if (checkPrime(i)) {
-        summa += i;
+        summa+=i;
         }
     }
     return summa;
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-//Copyright[2022] <NNTU-CS>
+//Copyright2022 <NNTU-CS>
 #include "alg.h"
 
 int main() {
