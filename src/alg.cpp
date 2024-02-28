d4553708d5ee09a6710f90462df9b9861e23245f
VadimKochnev <donskoy_kek@mail.ru>
2024-02-28 20:50:05 +0300

    add code

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,20 +1,69 @@
 // Copyright 2022 NNTU-CS
+#include <iostream>
 #include <cstdint>
+#include <cmath>
 #include "alg.h"
 
 
 bool checkPrime(uint64_t value) {
-// вставьте код функции
+	if (value == 1) {
+		return false;
+	}
+
+	if (value == 2 || value == 3) {
+		return true;
+	}
+
+    int sqrtValue = static_cast<int>(sqrt(value));
+
+    for (int i = 2; i <= sqrtValue; i++) {
+        if (value % i == 0) {
+            return false;
+        }
+    }
+
+    return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-// вставьте код функции
+	uint64_t currentNumber = 2;
+
+	while (n > 0) {
+		if (checkPrime(currentNumber)) {
+			n--;
+		}
+		currentNumber++;
+	}
+
+	return currentNumber - 1;
 }
 
 uint64_t nextPrime(uint64_t value) {
-// вставьте код функции
+	uint64_t currentNumber = value;
+	bool flag = false;
+
+	while (!flag) {
+		currentNumber++;
+
+		if (checkPrime(currentNumber)) {
+			flag = true;
+		}
+	}
+
+	return currentNumber;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-// вставьте код функции
+	uint64_t sum = 0;
+	uint64_t currentNumber = hbound;
+
+	while (currentNumber > 1) {
+		if (checkPrime(currentNumber)) {
+			sum += currentNumber;
+		}
+
+		currentNumber--;
+	}
+
+	return sum;
 }
