8eccf18ee0c0457cfcc41a8cbdb2eb68bfc6eaac
LisaKil <2005.lizak.05.01@gmail.com>
2024-02-28 16:20:33 +0300

    /

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,41 +1,36 @@
-// Copyright 2022 NNTU-CS
 #include <cstdint>
 #include "alg.h"
 
-
 bool checkPrime(uint64_t value) {
-    
-  if(value == 2 || value == 3){
+    if (value == 2 || value == 3) {
         return true;
     }
     uint64_t counter =0;
-    for(uint64_t i =2; i <= value/2; i++){
-        if(value % i == 0){
+    for (uint64_t i = 2; i <= value/2; i++) {
+        if (value % i == 0) {
             counter++;
         }
-      
-                            }
-          if(counter == 0 && value!=1){
-            return true;
-        }
-        else{
-            return false;
-        }
-                            
+    }
+    if (counter == 0 && value != 1) {
+        return true;
+    }
+    else {
+        return false;
+    }
 }
 
 uint64_t nPrime(uint64_t n) {
     uint64_t count = 0;
     uint64_t i = 1;
     uint64_t number;
-    while(count!= n){
+    while (count != n) {
         bool res =  checkPrime(i);
-        if(res==true){
+        if (res) {
             count++;
-            number=i;
+            number = i;
             i++;
         }
-        else{
+        else {
             i++;
             continue;
         }
@@ -44,30 +39,25 @@ uint64_t nPrime(uint64_t n) {
 }
 
 uint64_t nextPrime(uint64_t value) {
-  value++;
-    while(true){
+    value++;
+    while (true) {
         bool test = checkPrime(value);
-       if(test == true){
+        if (test) {
             return value;
-       }
-       else{
-           value++;
-       }
+        }
+        else {
+            value++;
+        }
     }
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-    uint64_t sum=0;
-    uint64_t n =0;
-     while(true){
-           if(nextPrime(n)>=hbound){
-           break;
-       }
-        n = nextPrime(n);
-        sum+=n ;
-     
-     }
-     
+    uint64_t sum = 0;
+    for (uint64_t i = 2; i < hbound; ++i) {
+        if (checkPrime(i)) {
+            sum += i;
+        }
+    }
     return sum;
 }
 
