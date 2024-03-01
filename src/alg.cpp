e0733ae0d972dca4a1f70105130315b6079dbf5b
maxim.sustaev <maxim.sustaev@yandex.ru>
2024-03-01 23:38:14 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,57 +4,55 @@
 
 bool checkPrime(uint64_t value)
 {
-	int valueInCoren = sqrt(value);
-	if (value <= 1)
-	{
-		return false;
-	}
-	for (int i = 2; i <= valueInCoren; i++)
-	{
-		if (value % i == 0)
-		{
-			return false;
-		}
-	}
-	return true;
-
+	  int valueInCoren = sqrt(value);
+	  if (value <= 1)
+	  {
+		  return false;
+	  }
+	  for (int i = 2; i <= valueInCoren; i++)
+	  {
+		  if (value % i == 0)
+		  {
+			  return false;
+		  }
+	  }
+	  return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-	int count = 0;
-	for (int i = 2; ; i++)
-	{
-		if (checkPrime(i) == true)
-		{
-			count += 1;
-		}
-		if (count == n)
-		{
-			return i;
-			break;
-		}
-	}
+	  int count = 0;
+	  for (int i = 2; ; i++)
+	  {
+		  if (checkPrime(i) == true)
+		  {
+			  count += 1;
+		  }
+		  if (count == n)
+		  {
+			  return i;
+			  break;
+		  }
+	  }
 }
 
 uint64_t nextPrime(uint64_t value) {
-	for (int i = value + 1; ; i++)
-	{
-		if (checkPrime(i) == true)
-		{
-			return i;
-			break;
-		}
-	}
+	  for (int i = value + 1; ; i++)
+	  {
+		  if (checkPrime(i) == true)
+		  {
+			  return i;
+			  break;
+		  }
+	  }
 }
-
 uint64_t sumPrime(uint64_t hbound) {
-	uint64_t sumOfPrime = 0;
-	for (int i = 2; i < hbound; i++)
-	{
-		if (checkPrime(i) == true)
-		{
-			sumOfPrime += i;
-		}
-	}
-	return sumOfPrime;
+	    uint64_t sumOfPrime = 0;
+	    for (int i = 2; i <= hbound; i++)
+	    {
+		    if (checkPrime(i) == true)
+		    {
+			    sumOfPrime += i;
+		    }
+	    }
+	    return sumOfPrime;
 }
