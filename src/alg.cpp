a5561abec78c7a5786225e0dc0654b556d5dff33
maxim.sustaev <maxim.sustaev@yandex.ru>
2024-03-01 23:24:48 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,24 +1,24 @@
-// Copyright 2022 NNTU-CS
 #include <cstdint>
 #include "alg.h"
 using namespace std;
 #include <cmath>
 
-bool checkPrime(uint64_t value) {
-  int valueInCoren = sqrt(value);
-  if (value <= 1)
-  {
-    return false;
-  }
-  for(int i = 2; i <= valueInCoren; i++)
-  {
-    if (value % i == 0)
-    {
-      return false;
-    }
-  }
-  return true;
-  
+bool checkPrime(uint64_t value)
+{
+	int valueInCoren = sqrt(value);
+	if (value <= 1)
+	{
+		return false;
+	}
+	for (int i = 2; i <= valueInCoren; i++)
+	{
+		if (value % i == 0)
+		{
+			return false;
+		}
+	}
+	return true;
+
 }
 
 uint64_t nPrime(uint64_t n) {
@@ -38,19 +38,19 @@ uint64_t nPrime(uint64_t n) {
 }
 
 uint64_t nextPrime(uint64_t value) {
-  for (int i = value + 1; ; i++)
-  {
-	  if (checkPrime(i) == true)
-	  {
-		  return i;
-		  break;
-	  }
-  }
+	for (int i = value + 1; ; i++)
+	{
+		if (checkPrime(i) == true)
+		{
+			return i;
+			break;
+		}
+	}
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-  uint64_t sumOfPrime = 0;
-	for (int i = 2; i <= hbound; i++)
+	uint64_t sumOfPrime = 0;
+	for (int i = 2; i < hbound; i++)
 	{
 		if (checkPrime(i) == true)
 		{
