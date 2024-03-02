a10901c11f083ee49aa3ce735752231d7a57d5ff
yonkoyo <91339395+yonkoyo@users.noreply.github.com>
2024-03-02 11:51:12 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,17 +4,52 @@
 
 
 bool checkPrime(uint64_t value) {
-// вставьте код функции
+    if (value < 2) return false;
+    if (value == 2) return true;
+    if (value % 2 == 0) return false;
+    
+    for (uint64_t i = 3; i * i <= value; i += 2) {
+        if (value % i == 0) {
+            return false;
+        }
+    }
+    
+    return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-// вставьте код функции
+    if (n == 1) return 2;
+    uint64_t count = 1;
+    uint64_t num = 3;
+    
+    while (count < n) {
+        if (checkPrime(num)) {
+            count++;
+        }
+        num += 2;
+    }
+    
+    return num - 2;
 }
 
 uint64_t nextPrime(uint64_t value) {
-// вставьте код функции
+    if (value < 2) return 2;
+    
+    uint64_t next = value + 1;
+    while (!checkPrime(next)) {
+        next++;
+    }
+    
+    return next;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-// вставьте код функции
+    uint64_t sum = 0;
+    for (uint64_t i = 2; i < hbound; ++i) {
+        if (checkPrime(i)) {
+            sum += i;
+        }
+    }
+    
+    return sum;
 }
