e9142b970f6dc48801d707657ef6d08946f43145
flapik19 <selivanovvlad3211@gmail.com>
2024-03-02 18:42:04 +0300

    fix

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,7 +6,7 @@ bool checkPrime(uint64_t value) {
     if (value <= 1)
         return false;
 
-    for (int i = 2; i * i < value + 1; i++){
+    for (int i = 2; i * i < value + 1; i++) {
         if (value % i == 0)
             return false;
     }
