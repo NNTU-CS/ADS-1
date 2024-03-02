e44e6d9624a749a86123009cba59d1a6ac413bb7
VadimKochnev <donskoy_kek@mail.ru>
2024-03-02 18:01:25 +0300

    b259

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,69 +1,68 @@
 // Copyright 2022 NNTU-CS
-#include <iostream>
-#include <cstdint>
 #include <cmath>
+#include <cstdint>
+
 #include "alg.h"
 
-
 bool checkPrime(uint64_t value) {
-	if (value == 1) {
-		return false;
-	}
+  if (value == 1) {
+    return false;
+  }
 
-	if (value == 2 || value == 3) {
-		return true;
-	}
-
-    int sqrtValue = static_cast<int>(sqrt(value));
-
-    for (int i = 2; i <= sqrtValue; i++) {
-        if (value % i == 0) {
-            return false;
-        }
-    }
-
+  if (value == 2 || value == 3) {
     return true;
+  }
+
+  int sqrtValue = static_cast<int>(sqrt(value));
+
+  for (int i = 2; i <= sqrtValue; i++) {
+    if (value % i == 0) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-	uint64_t currentNumber = 2;
+  uint64_t currentNumber = 2;
 
-	while (n > 0) {
-		if (checkPrime(currentNumber)) {
-			n--;
-		}
-		currentNumber++;
-	}
+  while (n > 0) {
+    if (checkPrime(currentNumber)) {
+      n--;
+    }
+    currentNumber++;
+  }
 
-	return currentNumber - 1;
+  return currentNumber - 1;
 }
 
 uint64_t nextPrime(uint64_t value) {
-	uint64_t currentNumber = value;
-	bool flag = false;
+  uint64_t currentNumber = value;
+  bool flag = false;
 
-	while (!flag) {
-		currentNumber++;
+  while (!flag) {
+    currentNumber++;
 
-		if (checkPrime(currentNumber)) {
-			flag = true;
-		}
-	}
+    if (checkPrime(currentNumber)) {
+      flag = true;
+    }
+  }
 
-	return currentNumber;
+  return currentNumber;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-	uint64_t sum = 0;
-	uint64_t currentNumber = hbound;
+  uint64_t sum = 0;
+  uint64_t currentNumber = hbound;
 
-	while (currentNumber > 1) {
-		if (checkPrime(currentNumber)) {
-			sum += currentNumber;
-		}
+  while (currentNumber > 1) {
+    if (checkPrime(currentNumber)) {
+      sum += currentNumber;
+    }
 
-		currentNumber--;
-	}
+    currentNumber--;
+  }
 
-	return sum;
+  return sum;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 // Copyright 2022 NNTU-CS
+#include <iostream>
 #include "alg.h"
+#include "alg.cpp"
 
 int main() {
     return 0;
