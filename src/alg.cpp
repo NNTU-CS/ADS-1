55f6eb585bcff46e24c70f4bb2a6927e77608c22
flapik19 <selivanovvlad3211@gmail.com>
2024-03-02 16:01:32 +0300

    b170

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,19 +2,44 @@
 #include <cstdint>
 #include "alg.h"
 
-
-bool checkPrime(uint64_t value) {
-// вставьте код функции
+bool checkPrime(uint64_t value) 
+{
+    for (int i = 2; i < sqrt(value) + 1; i ++){
+        if (value % i == 0){
+            return false;
+        }
+    }
+    return true;    
 }
 
 uint64_t nPrime(uint64_t n) {
-// вставьте код функции
+    int count = 0;
+    int i = 0;
+    while (count != n){
+        if (checkPrime(i)){
+            count ++;
+        }
+        i++;
+    }
+    return i;
 }
 
 uint64_t nextPrime(uint64_t value) {
-// вставьте код функции
+    int i = value + 1;
+    while (!checkPrime(i))
+    {
+        i++;
+    }
+    return i;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-// вставьте код функции
+    int i = 0;
+    int res = 0;
+    while (i<hbound){
+        if (checkPrime(i)){
+            res+=i;
+        }
+        i++;
+    }
 }
