36802058e2908364ce4885a113f01ed7ad4f73df
yonkoyo <91339395+yonkoyo@users.noreply.github.com>
2024-03-02 12:10:39 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,52 +4,52 @@
 
 
 bool checkPrime(uint64_t value) {
-    if (value < 2) return false;
-    if (value == 2) return true;
-    if (value % 2 == 0) return false;
+ if (value < 2) return false;
+ if (value == 2) return true;
+ if (value % 2 == 0) return false;
+
+ for (uint64_t i = 3; i * i <= value; i += 2) {
+  if (value % i == 0) {
+   return false;
+   }
+ }
     
-    for (uint64_t i = 3; i * i <= value; i += 2) {
-        if (value % i == 0) {
-            return false;
-        }
-    }
-    
-    return true;
+ return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-    if (n == 1) return 2;
-    uint64_t count = 1;
-    uint64_t num = 3;
+ if (n == 1) return 2; 
+ uint64_t count = 1;
+ uint64_t num = 3;
     
-    while (count < n) {
-        if (checkPrime(num)) {
-            count++;
-        }
-        num += 2;
-    }
+ while (count < n) {
+  if (CheckPrime(num)) {
+   count++;
+  }
+  num += 2;
+ }
     
-    return num - 2;
+ return num - 2;
 }
 
 uint64_t nextPrime(uint64_t value) {
-    if (value < 2) return 2;
+ if (value < 2) return 2;
     
-    uint64_t next = value + 1;
-    while (!checkPrime(next)) {
-        next++;
-    }
+ uint64_t next = value + 1;
+ while (!CheckPrime(next)) {
+  next++;
+ }
     
-    return next;
+ return next;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-    uint64_t sum = 0;
-    for (uint64_t i = 2; i < hbound; ++i) {
-        if (checkPrime(i)) {
-            sum += i;
-        }
-    }
+ uint64_t sum = 0;
+ for (uint64_t i = 2; i < hbound; ++i) {
+  if (CheckPrime(i)) {
+   sum += i;
+  }
+ }
     
-    return sum;
+ return sum;
 }
