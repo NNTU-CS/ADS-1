54ba937d2741df217c7c4b02e51be0d82a57ca4a
flapik19 <selivanovvlad3211@gmail.com>
2024-03-02 18:28:48 +0300

    fix

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,15 +5,12 @@
 bool checkPrime(uint64_t value)
 {
     if (value <= 1)
-    {
         return false;
-    }
+
     for (int i = 2; i * i < value + 1; i++)
     {
         if (value % i == 0)
-        {
             return false;
-        }
     }
     return true;
 }
@@ -22,9 +19,8 @@ uint64_t nPrime(uint64_t n)
 {
     int num = 0;
     for (int i = 0; i < n; i++)
-    {
         num = nextPrime(num);
-    }
+
     return num;
 }
 
@@ -32,18 +28,16 @@ uint64_t nextPrime(uint64_t value)
 {
     int i = value + 1;
     for (int i = value + 1;; i++)
-    {
         if (checkPrime(i))
             return i;
-    }
+
 }
 
 uint64_t sumPrime(uint64_t hbound)
 {
     uint64_t res = 0;
     for (uint64_t i = nextPrime(0); i < hbound; i = nextPrime(i))
-    {
         res += i;
-    }
+        
     return res;
 }
