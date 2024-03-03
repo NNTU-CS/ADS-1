cb601be85721f246d818763577358c0b1472dcf3
Kotyara21 <73665937+Kotyara21@users.noreply.github.com>
2024-03-03 20:49:35 +0300

    Add files via upload

---
+++ alg.cpp
@@ -0,0 +1,56 @@
+
+#include <iostream>
+#include <vector>
+
+bool checkPrime(uint64_t value) {
+    if (value < 2) {
+        return false;
+    }
+    for (uint64_t i = 2; i * i <= value; i++) {
+        if (value % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+uint64_t nPrime(uint64_t n) {
+    if (n == 1) {
+        return 2;
+    }
+
+    uint64_t count = 1;
+    uint64_t num = 3;
+
+    while (count < n) {
+        if (checkPrime(num)) {
+            count++;
+        }
+        if (count < n) {
+            num += 2;
+        }
+    }
+
+    return num;
+}
+
+uint64_t nextPrime(uint64_t value) {
+    uint64_t null = value + 1;
+    while (!checkPrime(null)) {
+        null++;
+    }
+
+    return null;
+}
+
+uint64_t sumPrime(uint64_t hbound) {
+    uint64_t sum = 0;
+
+    for (uint64_t i = 2; i < hbound; i++) {
+        if (checkPrime(i)) {
+            sum += i;
+        }
+    }
+
+    return sum;
+}
