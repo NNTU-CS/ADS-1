f50df4e0f3c2cf675b01da9a46bc039ddc2fe73d
Kotyara21 <73665937+Kotyara21@users.noreply.github.com>
2024-03-03 22:19:51 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,20 +4,20 @@
 
 bool checkPrime(uint64_t value) {
     if (value <= 1) {
-      return false;
+        return false;
     }
     for (uint64_t i = 2; i * i <= value; i++) {
-      if (value % i == 0) {
-        return false;
-      }
-      return true;
-  }
+        if (value % i == 0) {
+            return false;
+        }
+    }
+    return true;
 }
 uint64_t nPrime(uint64_t n) {
   if (n == 1) {
     return 2;
   }
-  uint54_t count = 1;
+  uint64_t count = 1;
   uint64_t num = 3;
   while (count < n) {
     if (checkPrime(num)) {
@@ -38,7 +38,7 @@ uint64_t nextPrime(uint64_t value) {
 }
 uint64_t sumPrime(uint64_t hbound) {
   uint64_t sum = 0;
-  for (uint64_t i =2; i < nbound; i++) {
+  for (uint64_t i =2; i < hbound; i++) {
     if (checkPrime(i)) {
       sum += i;
     }
