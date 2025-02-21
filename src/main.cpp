d8f5957bb3396bb952b95e4e891fc0ea50c23bb8
MisTerio497 <ipkovalenko2006@gmail.com>
2025-02-21 17:35:10 +0300

    Готовое задание

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,9 @@
 // Copyright 2022 NNTU-CS
+#include "../include/alg.h"
 #include <cstdlib>
 #include <iostream>
-#include "../include/alg.h"
 
-int main(int argc, char* argv[]) {
-    std::cout << twinPrimes(atoi(argv[1]),atoi(argv[2])) << std::endl;
-    return 0;
+int main(int argc, char *argv[]) {
+  std::cout << twinPrimes(atoi(argv[1]), atoi(argv[2])) << std::endl;
+  return 0;
 }
