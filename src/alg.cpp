0573917f869f84ddacd166ecf2f64d89690d4667
AnnaApatina <annaapatina2002@gmail.com>
2025-02-21 20:16:13 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,29 +1,58 @@
 // Copyright 2022 NNTU-CS
 #include <cstdint>
+#include <math.h>
 #include "alg.h"
 
-
 bool checkPrime(uint64_t value) {
-  // вставьте код функции
-  return true;
-}
-
-uint64_t nPrime(uint64_t n) {
-  // вставьте код функции
-  return 2;
+    for (uint64_t i = 2; i <= sqrt(value); i++) {
+        if (value % i == 0)
+            return false;
+    }
+    return true;
 }
 
 uint64_t nextPrime(uint64_t value) {
-  // вставьте код функции
-  return 2;
+    for (uint64_t i = value + 1; ; i++) {
+        if (checkPrime(i))
+            return i;
+    }
+}
+
+uint64_t nPrime(uint64_t n) {    
+    uint64_t count = 1, next = 2;
+    for (;;) {
+        if (count == n)
+            return next;
+        else {
+            next = nextPrime(next);
+            count++;
+        }
+    }
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-  // вставьте код функции
-  return 2;
+    u_int64_t summary = 0, current = 2;
+    for (; current < hbound;) {
+        summary += current;
+        current = nextPrime(current);
+    }
+    return summary;
 }
 
 uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
-  // вставьте код функции
-  return 1;
+    uint64_t count = 0, current = 2;
+    if (checkPrime(lbound))
+        current = lbound;
+    else
+        current  = nextPrime(lbound);
+    
+    for (; current < hbound;) {
+        uint64_t first = current;
+        uint64_t second = nextPrime(current);
+        if (second < hbound && (second - first) == 2) {
+            count++;
+            current = second;
+        }
+    }
+    return count;
 }
