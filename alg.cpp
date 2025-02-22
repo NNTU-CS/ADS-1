1956e093d5715d13c72d169bcccdb7bed040aa58
zed932 <mescheryakovsrg@gmail.com>
2025-02-22 19:21:57 +0300

    Add files via upload

--- alg.cpp
+++ alg.cpp
@@ -1,3 +1,4 @@
+// Copyright 2022 NNTU-CS
 #include <cstdint>
 #include <iostream>
 #include <cmath>
@@ -10,12 +11,13 @@ bool checkPrime(uint64_t value) {
   if (value == 2) {
     return true;
   }
-  int value_sqrt = static_cast<int>(std::sqrt(value));
+  int value_sqrt = (pow(value, 0.5) + 1) / 1;
   for (int i = 2; i <= value_sqrt; i++) {
     if (value % i == 0) {
       return false;
     }
   }
+  
   return true;
 }
 
@@ -28,17 +30,18 @@ uint64_t nPrime(uint64_t n) {
       prime_number = i;
     }
   }
+
   return prime_number;
 }
 
 uint64_t nextPrime(uint64_t value) {
   uint64_t next_prime = 0;
-  for (uint64_t i = value + 1; ; ++i) {
+  for (uint64_t i = value + 1; ; i++) {
     if (checkPrime(i)) {
       next_prime = i;
-      break;
     }
   }
+
   return next_prime;
 }
 
@@ -46,9 +49,10 @@ uint64_t sumPrime(uint64_t hbound) {
   uint64_t sum = 0;
   for (uint64_t i = 2; i < hbound; i++) {
     if (checkPrime(i)) {
-      sum += i;
+      sum = sum + i;
     }
   }
+
   return sum;
 }
 
@@ -59,5 +63,7 @@ uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
       twin_count++;
     }
   }
+
   return twin_count;
 }
+
