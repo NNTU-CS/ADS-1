8b381f116cda4853e1b5c0fa7529c2afd2c44aa0
Иван Коваленко <135465238+MisTerio497@users.noreply.github.com>
2025-02-24 14:23:58 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,12 @@
 // Copyright 2022 NNTU-CS
-#include "alg.h"
+#include "../include/alg.h"
 
 int main() {
+    int a = 10;
+    checkPrime(a);
+    nPrime(a);
+    nextPrime(a);
+    sumPrime(a);
+    twinPrimes(a, 21);
     return 0;
 }
