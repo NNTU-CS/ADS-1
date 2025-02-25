8be7525df323aa80b607d17d81155701efc0decb
munderr <mvyazov534@gmail.com>
2025-02-25 22:11:03 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,66 +5,66 @@
 
 
 bool checkPrime(uint64_t value) {
-    if (value < 2) {
-        return false;
+  if (value < 2) {
+    return false;
+  }
+  for (uint64_t i = 2; i <= sqrt(value); i++) {
+    if (value % i == 0) {
+      return false;
     }
-    for (uint64_t i = 2; i <= sqrt(value); i++) {
-        if (value % i == 0) {
-            return false;
-        }
-    }
-    return true;
+  }
+  return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-    if (n == 0) {
-        return 0;
+  if (n == 0) {
+    return 0;
+  }
+  uint64_t count = 0;
+  uint64_t num = 2;
+  while (true) {
+    if (checkPrime(num)) {
+      count++;
+      if (count == n) {
+        return num;
+      }
     }
-    uint64_t count = 0;
-    uint64_t num = 2;
-    while (true) {
-        if (checkPrime(num)) {
-            count++;
-            if (count == n) {
-                return num;
-            }
-        }
-        num++;
-    }
+    num++;
+  }
 }
 
 uint64_t nextPrime(uint64_t value) {
+  value++;
+  while (!checkPrime(value)) {
     value++;
-    while (!checkPrime(value)) {
-        value++;
-    }
-    return value;
+  }
+  return value;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-    uint64_t num = 2;
-    uint64_t sum = 0;
-    while (num < hbound) {
-        if (checkPrime(num)) {
-            sum += num;
-        }
-        num = nextPrime(num);
+  uint64_t num = 2;
+  uint64_t sum = 0;
+  while (num < hbound) {
+    if (checkPrime(num)) {
+      sum += num;
     }
-    return sum;
+    num = nextPrime(num);
+  }
+  return sum;
 }
 
 uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
-    uint64_t cnt = 0;
-    uint64_t num = nextPrime(lbound - 1);
-    while (num < hbound) {
-        uint64_t next = nextPrime(num);
-        if (next >= hbound) {
-            break;
-        }
-        if ((next - num) == 2) {
-            cnt++;
-        }
-        num = next;
+  uint64_t cnt = 0;
+  uint64_t num = nextPrime(lbound - 1);
+  while (num < hbound) {
+    uint64_t next = nextPrime(num);
+    if (next >= hbound) {
+      break;
     }
-    return cnt;
+    if ((next - num) == 2) {
+      cnt++;
+    }
+    num = next;
+  }
+  return cnt;
 }
