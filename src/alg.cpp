1a127a3a85366762a8f9fd8bc02b0bca6821667d
Eternity8c <morozkind6@gmail.com>
2025-02-25 20:45:44 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,53 +4,54 @@
 #include<cmath>
 
 bool checkPrime(uint64_t value) {
-  if (value < 2)
-    return false;
-  if (value % 2 == 0 && value > 2)
-    return false;
-  for (uint64_t i = 3; i <= sqrt(value); i += 2) {
-    if (value % i == 0) {
-      return false;
-    }
-  }
-  return true;
+	if (value < 2)
+		return false;
+	if (value % 2 == 0 && value > 2)
+		return false;
+	for (uint64_t i = 3; i <= sqrt(value); i += 2) { 
+		if (value % i == 0) {
+			return false;
+		}
+	}
+	return true;
 }
 
-uint64t nPrime(uint64t n) {
-  uint64_t col = 0;
-  uint64_t candidat = 1;
+uint64_t nPrime(uint64_t n) {
+	uint64_t col = 0;
+	uint64_t candidat = 1;
 
-  while (col < n) {
-    candidat++;
-    if (checkPrime(candidat)) {
-      col++;
-    }
-  }
-  return candidat;
+	while (col < n) {
+		candidat++;
+		if (checkPrime(candidat)) {
+			col++;
+		}
+	}
+	return candidat;
 }
 
-uint64t nextPrime(uint64t value) {
+uint64_t nextPrime(uint64_t value) {
 
-  if (value < 2) return 2;
+  if (value < 2){
+    return 2;
+  }
 
   uint64_t candidat = value;
-
 
   if (candidat % 2 == 0) {
     candidat++;
   } else {
-    candidat += 2; 
+    candidat += 2;
   }
 
   while (true) {
     if (checkPrime(candidat)) {
       return candidat;
     }
-    candidat += 2; 
+    candidat += 2;
   }
 }
 
-uint64t sumPrime(uint64t hbound) {
+uint64_t sumPrime(uint64_t hbound) {
   uint64_t sum = 0;
   uint64_t candidat = 1;
 
@@ -63,7 +64,7 @@ uint64t sumPrime(uint64t hbound) {
   return sum;
 }
 
-uint64t twinPrimes(uint64t lbound, uint64_t hbound) {
+uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
   uint64_t col = 0;
   uint64_t candidat = lbound;
 
