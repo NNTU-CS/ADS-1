fa740903ec8de5de09ec5f0df487f82eddb72f23
Павел Золин <pavelzolin@MacBook-Pro-Pavel.local>
2025-02-26 00:18:48 +0300

    Удалил папку build созданную мной

--- build/test/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/pavelzolin/Desktop/Project/ADS-1/test
-# Build directory: /Users/pavelzolin/Desktop/Project/ADS-1/build/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../_deps/googletest-build")
