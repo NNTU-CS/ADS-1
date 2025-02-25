16afec1ddff85a68efd8d3cfd672f052486a4138
Van4e7 <ivanzvectov@gmail.com>
2025-02-25 21:41:53 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -19,6 +19,7 @@ bool checkPrime(uint64_t value) {
     }
   }
   return true;
+  }
 
 uint64_t nPrime(uint64_t n) {
     uint64_t count = 1;
@@ -41,7 +42,7 @@ uint64_t nextPrime(uint64_t value) {
   }
 
 uint64_t sumPrime(uint64_t hbound) {
-  uint64_t sum = 0;
+    uint64_t sum = 0;
   for (uint64_t n = 2; n < hbound; n++) {
     if (checkPrime(n)) {
         sum += n;
