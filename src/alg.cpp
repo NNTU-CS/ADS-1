797262adccab496346d4d2ada1d3d78c03194569
Eternity8c <morozkind6@gmail.com>
2025-02-25 21:06:05 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,10 +4,12 @@
 #include<cmath>
 
 bool checkPrime(uint64_t value) {
-  if (value < 2)
+  if (value < 2){
     return false;
-  if (value % 2 == 0 && value > 2)
+  }
+  if (value % 2 == 0 && value > 2){
     return false;
+  }
   for (uint64_t i = 3; i <= sqrt(value); i += 2) {
     if (value % i == 0) {
       return false;
@@ -30,13 +32,16 @@ uint64_t nPrime(uint64_t n) {
 }
 
 uint64_t nextPrime(uint64_t value) {
-  if (value < 2) return 2;
+  if (value < 2){
+    return 2;
+  }
 
   uint64_t candidat = value;
 
   if (candidat % 2 == 0) {
     candidat++;
-  } else {
+  } 
+  else {
     candidat += 2;
   }
 
