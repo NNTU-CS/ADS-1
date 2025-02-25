af4dcac511b5e34b4c662e220fdee2ea988ae561
AnnaApatina <annaapatina2002@gmail.com>
2025-02-25 15:10:25 +0300

    ADS_1_3.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,35 +1,34 @@
 // Copyright 2022 NNTU-CS
+#include "alg.h"
 #include <cstdint>
 #include <math.h>
-#include "alg.h"
 
 bool checkPrime(uint64_t value) {
     for (uint64_t i = 2; i <= sqrt(value); i++) {
-        if (value % i == 0)
+        if (value % i == 0) {
             return false;
+        }
     }
     return true;
 }
-
 uint64_t nextPrime(uint64_t value) {
     for (uint64_t i = value + 1; ; i++) {
-        if (checkPrime(i))
+        if (checkPrime(i)) {
             return i;
+        }
     }
 }
-
-uint64_t nPrime(uint64_t n) {    
+uint64_t nPrime(uint64_t n) {
     uint64_t count = 1, next = 2;
     for (;;) {
-        if (count == n)
+        if (count == n) {
             return next;
-        else {
+        } else {
             next = nextPrime(next);
             count++;
         }
     }
 }
-
 uint64_t sumPrime(uint64_t hbound) {
     u_int64_t summary = 0, current = 2;
     for (; current < hbound;) {
@@ -38,25 +37,22 @@ uint64_t sumPrime(uint64_t hbound) {
     }
     return summary;
 }
-
 uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
     uint64_t count = 0, current = 2;
-    if (lbound >= hbound)
+    if (lbound >= hbound) {
         return 0;
-  
-    if (checkPrime(lbound))
+    }
+    if (checkPrime(lbound)) {
         current = lbound;
-    else
+    } else {
         current  = nextPrime(lbound);
-    
+    }
     while (current < hbound) {
         uint64_t first = current;
         uint64_t second = nextPrime(current);
-
         if (second >= hbound) {
             break;
         }
-      
         if (second - first == 2) {
             count++;
             current = second;
@@ -64,6 +60,5 @@ uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
             current = second;
         }
     }
-  
     return count;
 }
