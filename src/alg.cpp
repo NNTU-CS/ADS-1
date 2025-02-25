f64acb4a8def4f2afde3b15f7e484c1334fa649b
Eternity8c <morozkind6@gmail.com>
2025-02-25 20:15:46 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,29 +1,67 @@
 // Copyright 2022 NNTU-CS
 #include <cstdint>
 #include "alg.h"
-
+#include<cmath>
 
 bool checkPrime(uint64_t value) {
-  // вставьте код функции
-  return true;
+		if (value < 2)
+			return false;
+		if (value % 2 == 0 && value > 2)
+			return false;
+		for (uint64_t i = 3; i <= sqrt(value); i += 2) { 
+			if (value % i == 0) {
+				return false;
+			}
+		}
+		return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-  // вставьте код функции
-  return 2;
+		uint64_t col = 0;
+		uint64_t candidat = 1;
+
+		while (col < n) {
+			candidat++;
+			if (checkPrime(candidat)) {
+				col++;
+			}
+		}
+		return candidat;
 }
 
 uint64_t nextPrime(uint64_t value) {
-  // вставьте код функции
-  return 2;
+		uint64_t candidat = value;
+
+		while(true) {
+			candidat++;
+			if (checkPrime(candidat)) {
+				return candidat;
+			}
+		}
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-  // вставьте код функции
-  return 2;
+	uint64_t sum = 0;
+	uint64_t candidat = 1;
+
+	while (candidat < hbound) {
+		candidat++;
+		if (checkPrime(candidat)) {
+			sum += candidat;
+		}
+	}
+	return sum;
 }
 
 uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
-  // вставьте код функции
-  return 1;
+		uint64_t col = 0;
+		uint64_t candidat = lbound;
+
+		while (candidat < hbound-2) {
+			if (checkPrime(candidat) && checkPrime(candidat+2)) {
+				col++;
+			}
+			candidat++;
+		}
+		return col;
 }
