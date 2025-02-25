e17bfe62a4471ef944029295d53bb4aad1671ce2
Anitone <133121791+Anitone@users.noreply.github.com>
2025-02-25 17:45:27 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,53 +4,49 @@
 
 
 bool checkPrime(uint64_t value) {
-  // вставьте код функции
   int count = 0;
   for(int i = 1;i<=value;i++){
     if (value%i==0){count++;}
   }
-   if(count == 2){
-      return true;
-    }
-    else{
-      return false;
-    }
+  if(count == 2){
+    return true;
+  }
+  else{
+    return false;
+  }
 }
 
 uint64_t nPrime(uint64_t n) {
-  // вставьте код функции
   int number = 1;
   int count = 0;
   int index = 0;
   while (index != n) {
     for (int i = 1; i <= number; i++) {
-        if (number % i == 0) { count++; }
+      if (number % i == 0) { count++; }
     }
     if (count == 2) {
-        index++;
-        number++;
+      index++;
+      number++;
     }
     else {
-        number++;
+      number++;
     }
     count = 0;
   }
-
   return number-1;
 }
 
 uint64_t nextPrime(uint64_t value) {
-  // вставьте код функции
   int number = value + 1;
   int count = 0;
   while (true) {
     for (int i = 1; i <= number; i++) {
-        if (number % i == 0) {
-            count++;
-        }
+      if (number % i == 0) {
+        count++;
+      }
     }
     if (count == 2) {
-        break;
+      break;
     }
     number++;
     count = 0;
@@ -59,15 +55,14 @@ uint64_t nextPrime(uint64_t value) {
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-  // вставьте код функции
   int sum = 0;
   int count = 0;
   for (int index = 0; index < hbound; index++) {
     for (int i = 1; i <= index; i++) {
-        if (index % i == 0) { count++; }
+      if (index % i == 0) { count++; }
     }
     if (count == 2) {
-        sum += index;
+      sum += index;
     }
     count = 0;
   }
@@ -75,17 +70,16 @@ uint64_t sumPrime(uint64_t hbound) {
 }
 
 uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
-  // вставьте код функции
   int pairs = 0;
   int oldPrime = nextPrime(lbound);
   for (int index = lbound; index < hbound; index++) {
     int count = 0;
     for (int i = 1; i <= index; i++) {
-        if (index % i == 0) { count++; }
+      if (index % i == 0) { count++; }
     }
     if (count == 2) {
-        if (index - oldPrime == 2) { pairs++; }
-        oldPrime = index;
+      if (index - oldPrime == 2) { pairs++; }
+      oldPrime = index;
     }
   }
   return pairs;
