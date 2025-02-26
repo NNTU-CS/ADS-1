a6c08200cdeec5b16e1a537bbeb7c4c2e8f44013
perchig <ivanslepko11@gmail.com>
2025-02-26 15:22:47 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,12 +5,12 @@
 
 bool checkPrime(uint64_t value) {
   if (value == 0 || value == 1)
-    return false;
+  return false;
   if (value == 2)
-    return true;
-  for (uint64_t i = 2; i < value; i ++){
+  return true;
+  for (uint64_t i = 2; i < value; i++) {
     if (value % i == 0)
-      return false;
+    return false;
   }
   return true;
 }
@@ -19,17 +19,17 @@ uint64_t nPrime(uint64_t n) {
   uint64_t position = 0;
   uint64_t num = 0;
   while (position < n){
-    num ++;
+    num++;
     if (checkPrime(num))
-      position ++;
+    position++;
   }
   return num;
 }
 
 uint64_t nextPrime(uint64_t value) {
-uint64_t next = 0;
-  for (uint64_t i = value + 1; i < 2 * value; i++){
-    if (checkPrime(i)){
+  uint64_t next = 0;
+  for (uint64_t i = value + 1; i < 2 * value; i++) {
+    if (checkPrime(i)) {
       next = i;
       break;
     }
@@ -39,9 +39,8 @@ uint64_t next = 0;
 
 uint64_t sumPrime(uint64_t hbound) {
   uint64_t sum = 0;
-  for (uint64_t i = hbound - 1; i > 1; i -= 2)
-  {
-    if (checkPrime(i)){
+  for (uint64_t i = hbound - 1; i > 1; i -= 2) {
+    if (checkPrime(i)) {
       sum += i;
     }
   }
@@ -55,7 +54,7 @@ uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
   {
     if (checkPrime(twin) && checkPrime(twin + 2))
     {
-      count ++;
+      count++;
     }
   }
   return count;
