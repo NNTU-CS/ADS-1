1cbe142eba961eec9435a87a8ae607447284e482
Krek0204 <vlad020406@yandex.ru>
2025-02-26 20:02:24 +0300

    Update_1 alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -13,7 +13,7 @@ bool checkPrime(uint64_t value) {
   if (value == 2) {
     return true;
   }
-  for (int i = 3; i <= ((value / 2) + 1); i+2) {
+  for (int i = 3; i <= ((value / 2) + 1); i+=2) {
     if (value % i == 0) {
       return false;
     }
@@ -25,8 +25,8 @@ uint64_t nPrime(uint64_t n) {
   if (n == 1) {
     return 2;
   }
-  int count = 0;
-  for (uint64_t i = 3; ; i + 2) {
+  int count = 1;
+  for (uint64_t i = 3; ; i += 2) {
     if (checkPrime(i)) {
       count++;
     }
@@ -39,13 +39,13 @@ uint64_t nPrime(uint64_t n) {
 
 uint64_t nextPrime(uint64_t value) {
   if ((value % 2) == 0) {
-    for (uint64_t i = value + 1; ; i + 2) {
+    for (uint64_t i = value + 1; ; i += 2) {
       if (checkPrime(i)) {
         return i;
       }
     }
   } else {
-    for (uint64_t i = value; ; i + 2) {
+    for (uint64_t i = value; ; i += 2) {
       if (checkPrime(i)) {
         return i;
       }
@@ -62,7 +62,7 @@ uint64_t sumPrime(uint64_t hbound) {
   if (hbound == 3) {
     return 2;
   }
-  for (uint64_t i = 3; i < hbound; i + 2) {
+  for (uint64_t i = 3; i < hbound; i += 2) {
     if (checkPrime(i)) {
       sum += i;
     }
@@ -81,7 +81,7 @@ uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
       }
     }
     if (first_prime == 2) {
-      for (uint64_t i = first_prime + 1; i < hbound; i + 2) {
+      for (uint64_t i = first_prime + 1; i < hbound; i += 2) {
         if (checkPrime(i)) {
           if (i - first_prime == 2) {
             count++;
@@ -90,7 +90,7 @@ uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
         }
       }
     } else {
-      for (uint64_t i = first_prime; i < hbound; i + 2) {
+      for (uint64_t i = first_prime; i < hbound; i += 2) {
         if (checkPrime(i)) {
           if (i - first_prime == 2) {
             count++;
@@ -107,7 +107,7 @@ uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
       }
     }
     if (first_prime == 2) {
-      for (uint64_t i = first_prime + 1; i < hbound; i + 2) {
+      for (uint64_t i = first_prime + 1; i < hbound; i += 2) {
         if (checkPrime(i)) {
           if (i - first_prime == 2) {
             count++;
@@ -117,7 +117,7 @@ uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
       }
     }
     else {
-      for (uint64_t i = first_prime; i < hbound; i + 2) {
+      for (uint64_t i = first_prime; i < hbound; i += 2) {
         if (checkPrime(i)) {
           if (i - first_prime == 2) {
             count++;
