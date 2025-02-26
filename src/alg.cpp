1f29397cc1edb37c17520af0eec16dd665891e41
GLUKERZZZ <artem2801200635@gmail.com>
2025-02-26 21:23:09 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,8 +6,7 @@
 bool checkPrime(uint64_t value) {
   if (value < 2) return false; 
   if (value == 2) return true; 
-  if (value % 2 == 0) return false; 
-
+  if (value % 2 == 0) return false;
   for (uint64_t i = 3; i * i <= value; i += 2) {
     if (value % i == 0) {
       return false; 
@@ -15,26 +14,20 @@ bool checkPrime(uint64_t value) {
   }
   return true;
 } 
-
 uint64_t nPrime(uint64_t n) {
   if (n < 1) return 0; 
-
   uint64_t count = 0; 
   uint64_t num = 1; 
-
   while (count < n) {
     num++;
     bool isPrime = (num > 1); 
-
     for (uint64_t i = 2; isPrime && (i * i <= num); i++) {
       if (num % i == 0) isPrime = false; 
     }
-
     if (isPrime) count++;
   }
   return num; 
-} 
-
+}
 uint64_t nextPrime(uint64_t value) {
   if (value < 2) return 2; // Первое простое число
   while (true) {
@@ -49,7 +42,6 @@ uint64_t nextPrime(uint64_t value) {
     if (isPrime) return value;
   }
 } 
-
 uint64_t sumPrime(uint64_t hbound) {
   uint64_t sum = 0;
   for (uint64_t num = 2; num < hbound; num++) {
@@ -64,7 +56,6 @@ uint64_t sumPrime(uint64_t hbound) {
   }
   return sum;
 } 
-
 uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
   uint64_t count = 0;
 
@@ -80,3 +71,4 @@ uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
   }
   return count;
 }
+
