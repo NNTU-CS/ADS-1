53e309c070d1929b88c6630a6984b78a97572eda
NastyaIlina07 <149273255+NastyaIlina07@users.noreply.github.com>
2025-02-26 16:43:35 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,53 +6,53 @@
 
 bool checkPrime(uint64_t value) {
     if (value < 2) {
-        return false;
-    }
+    return false;
+  }
     for (uint64_t i = 2; i * i <= value; ++i) {
         if (value % i == 0) {
-            return false;
-        }
+      return false;
     }
-    return true;
+  }
+  return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-    uint64_t num = 2;
-    uint64_t count = 0;
+  uint64_t num = 2;
+  uint64_t count = 0;
     while (count < n) {
         if (checkPrime(num)) {
-            count += 1;
-        }
-        num++;
+      count += 1;
     }
-    return num - 1;
+    num++;
+  }
+  return num - 1;
 }
 
 uint64_t nextPrime(uint64_t value) {
-    uint64_t next = value + 1;
+  uint64_t next = value + 1;
     while (!checkPrime(next)) {
-        next++;
-    }
-    return next;
+    next++;
+  }
+  return next;
 }
 
 uint64_t sumPrime(uint64_t rbound) {
     if (rbound < 2) {
-        return 0;
-    }
+    return 0;
+  }
 
-    uint64_t sum = 0;
+  uint64_t sum = 0;
     for (uint64_t num = 2; num < rbound; num = nextPrime(num)) {
-        sum += num;
-    }
-    return sum;
+    sum += num;
+  }
+  return sum;
 }
 
 uint64_t twinPrimes(uint64_t lbound, uint64_t rbound) {
-    uint64_t count = 0;
+  uint64_t count = 0;
     for (uint64_t i = lbound; i < rbound; i = nextPrime(i)) {
-        if (nextPrime(i) - i == 2) count++;
-    }
-    return count;
+    if (nextPrime(i) - i == 2) count++;
+  }
+  return count;
 }
 
