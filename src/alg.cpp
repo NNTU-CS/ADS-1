b9d47ee7eeefdfb356b1a7ae6e7f8cac0b6833f4
Axeber0 <axeber.yt@gmail.com>
2025-02-27 16:02:44 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,54 +1,47 @@
 // Copyright 2022 NNTU-CS
 #include <cstdint>
 #include "alg.h"
-#include <math.h>
+
 bool checkPrime(uint64_t value) {
-  for (uint64_t k = 2; k <= sqrt(value); k++) {
-      if (value != 1) {
-      if ((value % k == 0)) {
-        return false;
-      }
-      else {
-        return false;
-      }
-    }
+  if (value == 1) return false; 
+  for (uint64_t k = 2; k*k <= value; k++) {
+      if (value % k == 0) return false;
   }
   return true;
 }
 uint64_t nPrime(uint64_t n) {
-  int count = 0;
-  uint64_t res = 0;
-  uint64_t j = 0;
-  for (j = 2; j < pow(n, 2); j++) {
-    if (checkPrime(j) == true) count++;
-    if (count == n) res = j;
+  uint64_t next = 1;
+  uint64_t count = 0;
+  while (count != n){
+      next += 1;
+      if (checkPrime(next)) count++;
   }
-  return res;
+  return next;
 }
 uint64_t nextPrime(uint64_t value) {
-  uint64_t count = 0;
-  for (uint64_t m = value; m < pow(value, 2); m++) {
-    if (checkPrime(m) == true) count = m;
+  uint64_t counter = value + 1;
+  while (checkPrime(counter) == false) {
+      counter++;
   }
-  return count;
+  return counter;
 }
 uint64_t sumPrime(uint64_t hbound) {
-  int sum = 0;
-  uint64_t res = 0;
-  int count = 0;
-  for (int j = 2; j < pow(hbound, 2); j++) {
-    if (checkPrime(j) == true) {
+  uint64_t sum = 0;
+  uint64_t count = 2;
+  while (count != hbound) {
+      if (checkPrime(count) == true) {
+          sum += count;
+      }
       count++;
-      sum += j;
-      if (count == hbound) res = sum;
-    }
   }
   return sum;
 }
 uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
-  int count = 0;
-  for (int i = lbound; i <= hbound; i++) {
-    if ((checkPrime(i) == true) && (checkPrime(i + 2) == true)) count++;
+  uint64_t per = lbound;
+  uint64_t count = 0;
+  while (per != hbound) {
+      if ((checkPrime(per) == true) && (checkPrime(per + 2) == true)) count++;
+      per++;
   }
-  return count;
+  return count - 1;
 }
