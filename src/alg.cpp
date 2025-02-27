594ac9b9ac7e73342f3ff3d67b77f16e79500830
Sony Podrazska <146202175+Podrazik@users.noreply.github.com>
2025-02-27 14:39:26 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,16 +1,20 @@
 // Copyright 2022 NNTU-CS
 #include <cstdint>
-#include "alg.h"
 #include <cmath>
+#include "alg.h"
+
 
 
 bool checkPrime(uint64_t value) {
-  if (value<=1){
+  if (value <= 1) {
         return false;
     }
-  for (size_t i = 2; i < (sqrt(value))+1; i++)
+  if (value == 2) {
+      return true;
+  }
+  for (size_t i = 2; i < (sqrt(value)) + 2; i++)
   {
-      if ((value%i==0)){return false;}
+      if ((value % i == 0)){return false;}
   }
   return true;
 }
@@ -19,7 +23,7 @@ uint64_t nPrime(uint64_t n) {
   int num = 1;
   for (size_t i = 2; i <= n; i++)
   {
-      if (checkPrime(i) == true){
+      if (checkPrime(i) == true) {
           num = num+1;
       }   
   }
@@ -27,19 +31,19 @@ uint64_t nPrime(uint64_t n) {
 }
 
 uint64_t nextPrime(uint64_t value) {
-  for (size_t i = value+1; i < (value*value); i++)
-    {
-        if (checkPrime(i) == true){
-            return i;
-        }
-    }
-    return 2;
+  for (size_t i = value + 1; i < (value * value); i++)
+  {
+      if (checkPrime(i) == true) {
+          return i;
+      }
+  }
+  return 2;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
   int first = nextPrime(0);
   int sum = first;
-  while(nextPrime(first) < hbound){
+  while (nextPrime(first) < hbound) {
       sum = sum + nextPrime(first);
       first= nextPrime(first);
   }
@@ -50,7 +54,7 @@ uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
   int count = 0;
   for (size_t i = lbound; i < hbound; i++)
   {
-      if(checkPrime(i) == true && nextPrime(i)-i == 2){
+      if (checkPrime(i) == true && nextPrime(i) - i == 2){
           count +=1;
       }
   }
