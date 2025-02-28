71ed0863f91f677c5ee8243f622538071e78c4f8
methevodichka <145225062+methevodichka@users.noreply.github.com>
2025-02-28 15:02:20 +0300

    Update alg.cpp 2

--- src/alg.cpp
+++ src/alg.cpp
@@ -28,16 +28,31 @@ uint64_t nPrime(uint64_t n) {
 }
 
 uint64_t nextPrime(uint64_t value) {
-  // вставьте код функции
+  while (1) {
+    value++;
+    if (checkPrime(value)) {
+      return value;
+    }
+  }
   return 2;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-  // вставьте код функции
-  return 2;
+  uint64_t sum = 0;
+  for (uint64_t i = 2; i < hbound; i++) {
+    if (checkPrime(i)) {
+      sum += i;
+    }
+  }
+  return sum;
 }
 
 uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
-  // вставьте код функции
-  return 1;
+  uint64_t count = 0;
+  for (uint64_t i = lbound; i < hbound; i++) {
+    if (checkPrime(i) && checkPrime(i + 2) && (i + 2) < hbound) {
+      count++;
+    }
+  }
+  return count;
 }
