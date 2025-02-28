21c6fb0624ccefe8c439acff097b2566e6137e9c
VolkovArseniy2006 <156698320+VolkovArseniy2006@users.noreply.github.com>
2025-02-28 18:49:43 +0300

    АААААААААА
    АААААААААА

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,14 +3,13 @@
 #include "alg.h"
 
 bool checkPrime(uint64_t value) {
-    if (value <= 1) return false;
-    if (value == 2) return true;
-    if (value % 2 == 0) return false;
-
-    for (uint64_t i = 3; i * i <= value; i += 2) {
-        if (value % i == 0) return false;
-    }
-    return true;
+  if (value <= 1) return false;
+  if (value == 2) return true;
+  if (value % 2 == 0) return false;
+  for (uint64_t i = 3; i * i <= value; i += 2) {
+      if (value % i == 0) return false;
+  }
+  return true;
 }
 
 uint64_t nPrime(uint64_t n) {
