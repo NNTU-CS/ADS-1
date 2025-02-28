71afe754ef1b0f5435a873146dec997d747b76ee
VolkovArseniy2006 <156698320+VolkovArseniy2006@users.noreply.github.com>
2025-02-28 18:38:08 +0300

    Пробую ещё.
    Пробелы-пробелы-пробелы-пробелы...

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,52 +4,50 @@
 
 //№ 1
 bool checkPrime(uint64_t value) {
-  if (value <= 1) return false;
-  if (value == 2) return true;
-  if (value % 2 == 0) return false;
-
-  for (uint64_t i = 3; i * i <= value; i += 2) {
-    if (value % i == 0) return false;
-  }
-  return true;
+    if (value <= 1) return false;
+    if (value == 2) return true;
+    if (value % 2 == 0) return false;
+    for (uint64_t i = 3; i * i <= value; i += 2) {
+        if (value % i == 0) return false;
+    }
+    return true;
 }
 
 
 //№ 2
 uint64_t nPrime(uint64_t n) {
-  uint64_t count = 0;
-  uint64_t num = 2;
-
-  while (true) {
+    uint64_t count = 0;
+    uint64_t num = 2;
+    while (true) {
     if (checkPrime(num)) {
-      count++;
-      if (count == n) return num;
+        count++;
+        if (count == n) return num;
     }
     num++;
-  }
+    }
 }
 
 //№ 3
 uint64_t nextPrime(uint64_t value) {
-  value++;
-  while (!checkPrime(value)) value++;
-  return value;
+    value++;
+    while (!checkPrime(value)) value++;
+    return value;
 }
 
 //№ 4
 uint64_t sumPrime(uint64_t hbound) {
-  uint64_t sum = 0;
-  for (uint64_t i = 2; i < hbound; i++) {
+    uint64_t sum = 0;
+    for (uint64_t i = 2; i < hbound; i++) {
     if (checkPrime(i)) sum += i;
-  }
-  return sum;
+    }
+    return sum;
 }
 
 //№ 5
 uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
-  uint64_t count = 0;
-  for (uint64_t i = lbound; i < hbound - 1; i++) {
+    uint64_t count = 0;
+    for (uint64_t i = lbound; i < hbound - 1; i++) {
     if (checkPrime(i) && checkPrime(i + 2)) count++;
-  }
-  return count;
+    }
+    return count;
 }
