11df59525d5520a88352c9f137f634c3ed700e1b
nefedovat <ntanya54321@gmail.com>
2025-02-28 18:35:00 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -9,7 +9,7 @@ bool checkPrime(uint64_t value) {
   if (value % 2 == 0) return false;
   for (uint64_t i = 3; value >= i*i; i += 2) {
     if (value % i == 0)
-      return false;
+    return false;
   }
   return true;
 }
@@ -20,7 +20,7 @@ uint64_t nPrime(uint64_t n) {
   while (count < n) {
     num++;
     if (checkPrime(num))
-      count++;
+    count++;
   }
   return num;
 }
@@ -36,7 +36,7 @@ uint64_t sumPrime(uint64_t hbound) {
   uint64_t sum = 0;
   for (uint64_t i = 2; i < hbound; i++) {
     if (checkPrime(i))
-      sum += i;
+    sum += i;
   }
   return sum;
 }
@@ -45,7 +45,7 @@ uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
   uint64_t count = 0;
   for (uint64_t i = lbound; i < hbound - 1; i++) {
     if (checkPrime(i) && checkPrime(i + 2))
-      count++;
+    count++;
   }
   return count;
 }
