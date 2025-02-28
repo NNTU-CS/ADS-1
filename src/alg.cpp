45b6e8e6747aeb87882c8f7f8ec171cb9aa29ce7
lesh <113383963+ilyaLeshenko@users.noreply.github.com>
2025-02-28 23:41:31 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -19,40 +19,40 @@ uint64_t nPrime(uint64_t n) {
   uint64_t check = 0;
   uint64_t* res = new uint64_t[n]{ 0 };
   while (count < n) {
-    check++;
+      check++;
     if (checkPrime(check)) {
-      res[count] = check;
-      count++;
+        res[count] = check;
+        count++;
+      }
     }
+    uint64_t result = res[n-1];
+    return result;
   }
-  uint64_t result = res[n-1];
-  return result;
-}
 
 uint64_t nextPrime(uint64_t value) {
-  value++;
-  while (!checkPrime(value)) {
     value++;
+  while (!checkPrime(value)) {
+      value++;
+    }
+    return value;
   }
-  return value;
-}
 
 uint64_t sumPrime(uint64_t hbound) {
-  uint64_t sum = 0;
+    uint64_t sum = 0;
   for (int i = 0; i < hbound; i++) {
     if (checkPrime(i)) {
-      sum += i;
+        sum += i;
+      }
     }
+    return sum;
   }
-  return sum;
-}
 
 uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
-  uint64_t count = 0;
+    uint64_t count = 0;
   for (uint64_t i = lbound; i < hbound-2; i++) {
     if (checkPrime(i) && checkPrime(i + 2)) {
-      count++;
+        count++;
+      }
     }
+    return count;
   }
-  return count;
-}
