8897bcff2ff51f366f5a3cb09bbf92497ba88087
VolkovArseniy2006 <156698320+VolkovArseniy2006@users.noreply.github.com>
2025-02-28 18:43:53 +0300

    Пробую.
    АХАХАХАХАХАХ

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,20 +2,17 @@
 #include <cstdint>
 #include "alg.h"
 
-//№ 1
-bool checkPrime(uint64_t value) {
+bool checkPrime(uint64_t value) {  //№ 1
     if (value <= 1) return false;
     if (value == 2) return true;
     if (value % 2 == 0) return false;
     for (uint64_t i = 3; i * i <= value; i += 2) {
         if (value % i == 0) return false;
-    }
+      }
     return true;
 }
 
-
-//№ 2
-uint64_t nPrime(uint64_t n) {
+uint64_t nPrime(uint64_t n) {  //№ 2
     uint64_t count = 0;
     uint64_t num = 2;
     while (true) {
@@ -27,15 +24,13 @@ uint64_t nPrime(uint64_t n) {
     }
 }
 
-//№ 3
-uint64_t nextPrime(uint64_t value) {
+uint64_t nextPrime(uint64_t value) {  //№ 3
     value++;
     while (!checkPrime(value)) value++;
     return value;
 }
 
-//№ 4
-uint64_t sumPrime(uint64_t hbound) {
+uint64_t sumPrime(uint64_t hbound) {  //№ 4
     uint64_t sum = 0;
     for (uint64_t i = 2; i < hbound; i++) {
     if (checkPrime(i)) sum += i;
@@ -43,8 +38,7 @@ uint64_t sumPrime(uint64_t hbound) {
     return sum;
 }
 
-//№ 5
-uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
+uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {  //№ 5
     uint64_t count = 0;
     for (uint64_t i = lbound; i < hbound - 1; i++) {
     if (checkPrime(i) && checkPrime(i + 2)) count++;
