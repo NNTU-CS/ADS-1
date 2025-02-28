f3ec50678dd111875f6811eddc67ce0e04fee694
nastyanebuch12 <145349015+nastyanebuch12@users.noreply.github.com>
2025-03-01 00:48:15 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,26 +4,50 @@
 
 
 bool checkPrime(uint64_t value) {
-  // вставьте код функции
+  for (int i = 2; i < (value + 2) / 2; i++) {
+    if (value % i == 0) {
+      return false;
+    }
+  }
   return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-  // вставьте код функции
-  return 2;
+  int value = 2;
+  int val_num = 1;
+  while (val_num < n) {
+    value++;
+    if (checkPrime(value) == true) {
+      val_num++;
+    }
+  }
+  return value;
 }
 
 uint64_t nextPrime(uint64_t value) {
-  // вставьте код функции
-  return 2;
+  value++;
+  while (checkPrime(value) != true) {
+    value++;
+  }
+  return value;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-  // вставьте код функции
-  return 2;
+  int value = 2;
+  int sum = 0;
+  while (value < hbound) {
+    sum += value;
+    value = nextPrime(value);
+  }
+  return sum;
 }
 
 uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
-  // вставьте код функции
-  return 1;
+  int count = 0;
+  for (int value = lbound; value < hbound; value = nextPrime(value)) {
+    if ((nextPrime(value) < hbound) && (nextPrime(value) - value == 2)) {
+      count++;
+    }
+  }
+  return count;
 }
