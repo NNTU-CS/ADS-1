25dda64e596f761a94acfd4c38531de445148af3
VolkovArseniy2006 <156698320+VolkovArseniy2006@users.noreply.github.com>
2025-02-28 18:53:18 +0300

    А, Боже!
    Пожалуйста!

--- src/alg.cpp
+++ src/alg.cpp
@@ -19,29 +19,29 @@ uint64_t nPrime(uint64_t n) {
     if (checkPrime(num)) {
       count++;
       if (count == n) return num;
-      }
+    }
     num++;
   }
 }
 
 uint64_t nextPrime(uint64_t value) {
-    value++;
-    while (!checkPrime(value)) value++;
-    return value;
+  value++;
+  while (!checkPrime(value)) value++;
+  return value;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-    uint64_t sum = 0;
-    for (uint64_t i = 2; i < hbound; i++) {
-        if (checkPrime(i)) sum += i;
-    }
-    return sum;
+  uint64_t sum = 0;
+  for (uint64_t i = 2; i < hbound; i++) {
+    if (checkPrime(i)) sum += i;
+  }
+  return sum;
 }
 
 uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
-    uint64_t count = 0;
-    for (uint64_t i = lbound; i < hbound - 1; i++) {
-        if (checkPrime(i) && checkPrime(i + 2)) count++;
-    }
-    return count;
+  uint64_t count = 0;
+  for (uint64_t i = lbound; i < hbound - 1; i++) {
+    if (checkPrime(i) && checkPrime(i + 2)) count++;
+  }
+  return count;
 }
