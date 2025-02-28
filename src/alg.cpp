57b5289b921e0fae860b25e429cc0ba01ac1567c
Egor Mitrofanov <82968933+MaybeeYou@users.noreply.github.com>
2025-02-28 15:02:46 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,7 +4,24 @@
 
 
 bool checkPrime(uint64_t value) {
-  // вставьте код функции
+  if (value <= 1) {
+    return false;
+  }
+
+  if (value == 2) {
+    return true;
+  }
+
+  if (value % 2 == 0) {
+    return false;
+  }
+
+  for (uint64_t i = 3; i * i <= value; i += 2) {
+    if (value % i == 0) {
+      return false; 
+    }
+  }
+  return true;
   return true;
 }
 
