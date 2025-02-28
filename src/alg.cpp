afdb309f12e16a9a8d3fd41918dd468dd68ea3dd
lesh <113383963+ilyaLeshenko@users.noreply.github.com>
2025-02-28 23:31:15 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,11 +4,13 @@
 
 
 bool checkPrime(uint64_t value) {
-  if (value < 2)
+  if (value < 2){
     return false;
-  for (int i = 2; i*i <=value ; i++)
+  }
+  for (int i = 2; i*i <=value ; i++){
     if (value % i == 0)
-      return false;
+    return false;
+  }
   return true;
 }
 
@@ -37,17 +39,20 @@ uint64_t nextPrime(uint64_t value) {
 
 uint64_t sumPrime(uint64_t hbound) {
   uint64_t sum = 0;
-  for (int i = 0; i < hbound; i++)
-    if (checkPrime(i))
+  for (int i = 0; i < hbound; i++){
+    if (checkPrime(i)){
       sum += i;
+    }
+  }
   return sum;
 }
 
 uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
   uint64_t count = 0;
   for (uint64_t i = lbound; i < hbound-2; i++) {
-    if (checkPrime(i) && checkPrime(i + 2))
+    if (checkPrime(i) && checkPrime(i + 2)){
       count++;
+    }
   }
   return count;
 }
