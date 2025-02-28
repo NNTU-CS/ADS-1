fc2a09cc172f8fe423641f4a63ba56c7a4a27a2e
Andrey20-07 <mr.klach999@gmail.com>
2025-02-28 14:21:19 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -27,21 +27,16 @@ uint64_t nPrime(uint64_t n) {
   if (n < 1) {
     return 0;
   }
-  if (n == 1) {
-    return 2;
-  }
-  if (n > 1) {
-    uint64_t erin = 1;
-    uint64_t andrey = 2;
-    while(1) {
-      if(checkPrime(andrey)) {
-        if (erin == n) {
-          return andrey;
-        }
-        erin++;
+  uint64_t erin = 1;
+  uint64_t andrey = 2;
+  while (1) {
+    if (checkPrime(andrey)) {
+      if (erin == n) {
+        return andrey;
       }
-      andrey++;
+      erin++;
     }
+    andrey++;
   }
 }
 
@@ -49,15 +44,10 @@ uint64_t nextPrime(uint64_t value) {
   if (value < 1) {
     return 0;
   }
-  if (value == 1) {
-    return 2;
-  }
-  if (value > 1) {
-    while(1){
-      value++;
-      if (checkPrime(value)) {
-        return value;
-      }
+  while (1) {
+    value++;
+    if (checkPrime(value)) {
+      return value;
     }
   }
 }
@@ -72,17 +62,15 @@ uint64_t sumPrime(uint64_t hbound) {
 }
 
 uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
-  if ((lbound < 3) || (lbound < hbound)){
+  if (lbound < hbound) {
     return 0;
   }
-  if (lbound >= 3) {
-    uint64_t schet = 0;
-    while(lbound != hbound-2) {
-      if ((checkPrime(lbound) == 1) && (checkPrime(lbound+2) == 1)) {
-        schet++;
-      }
-      lbound++;
+  uint64_t schet = 0;
+  while (lbound != hbound-2) {
+    if ((checkPrime(lbound) == 1) && (checkPrime(lbound+2) == 1)) {
+      schet++;
     }
-    return schet++;
+    lbound++;
   }
+  return schet++;
 }
