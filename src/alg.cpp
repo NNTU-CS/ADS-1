7a57ac4c50ee879a0372a2ad922c361453f5f62f
Анна <sol0dova.anna@yandex.ru>
2025-03-01 13:27:50 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,26 +4,53 @@
 
 
 bool checkPrime(uint64_t value) {
-  // вставьте код функции
+  if (value == 0 || value == 1)
+    return false;
+  for (int i = 2; i < value / 2 + 1; i++) {
+    if (value % i == 0)
+      return false;
+  }
   return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-  // вставьте код функции
-  return 2;
+  uint64_t count = 0; 
+  uint64_t currNum = 1; 
+  while (count < n) {
+    currNum ++;
+    if (checkPrime(currNum)) {
+      count++;
+    }
+  }
+  return currNum;
 }
 
 uint64_t nextPrime(uint64_t value) {
-  // вставьте код функции
-  return 2;
+  value++;
+  while (!checkPrime(value)) {
+    value++;
+  }
+  return value;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-  // вставьте код функции
-  return 2;
+  uint64_t sum = 0;
+  for (uint64_t i = 2; sum < hbound;i++) {
+    if (checkPrime(i) ) {
+      sum += i;
+    }
+    if (sum >= hbound) {
+      return sum - i;
+    }
+  }
+  return sum;
 }
 
 uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
-  // вставьте код функции
-  return 1;
+  uint64_t count = 0;
+  for (lbound; lbound < hbound;lbound++) {
+    if (checkPrime(lbound) && checkPrime(lbound + 2))
+      count++;
+  }
+  return count++;
 }
