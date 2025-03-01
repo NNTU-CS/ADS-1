76c0101fdd7cc1980ffdd058510af692458b5980
amidamAru <ivanandreivi4@gmail.com>
2025-03-01 10:01:39 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -23,10 +23,9 @@ bool checkPrime(uint64_t value) {
 
 uint64_t nPrime(uint64_t n) {
   if (n == 1)
-    return 2;
+return 2;
   uint64_t count = 1;
-  uint64_t i = 3;  
-
+  uint64_t i = 3;
   while (count < n) {
     if (checkPrime(i)) {
       count++;
@@ -34,7 +33,7 @@ uint64_t nPrime(uint64_t n) {
     if (count == n) {
       return i;
     }
-    i += 2; лу.
+    i += 2;
   }
   return 0; 
 }
@@ -55,12 +54,10 @@ uint64_t nextPrime(uint64_t value) {
 
 uint64_t sumPrime(uint64_t hbound) {
   uint64_t sum = 0;
-  if(hbound >= 2)
-  {
+  if(hbound >= 2){
     sum +=2;
   }
-  for (uint64_t n = 3; n <= hbound; n+=2) 
-    {
+  for (uint64_t n = 3; n <= hbound; n+=2) {
       if(checkPrime(n))
         sum += n;
     }
@@ -73,7 +70,6 @@ uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
     for (uint64_t i = lbound; i < hbound - 2; ++i) {
       if (i < 2)
         continue; 
-
         if (checkPrime(i) && checkPrime(i + 2)) {
             count++;
         }
