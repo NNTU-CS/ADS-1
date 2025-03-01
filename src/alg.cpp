d9b24f854571ad8032612b15f7d75ab003c83175
KorobovaKate <kat3kor@yandex.ru>
2025-03-01 20:27:57 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,26 +4,56 @@
 
 
 bool checkPrime(uint64_t value) {
-  // вставьте код функции
+  if (value < 2) return false;
+  for (uint64_t i = 2; i * i <= value; i++) {
+    if (value % i == 0) return false;
+  }
   return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-  // вставьте код функции
-  return 2;
+  uint64_t kol = 0;
+  uint64_t num = 0;
+  
+  if (n < 2) return 0;
+  while (kol < n) {
+    num++;
+    if (checkPrime(num)) {
+      kol++;
+    }
+  }
+  return num;
 }
 
 uint64_t nextPrime(uint64_t value) {
-  // вставьте код функции
-  return 2;
+  value++;
+  while(!checkPrime(value)) {
+    value++;
+  }
+  return value;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-  // вставьте код функции
-  return 2;
+  uint64_t sum = 0;
+  for (uint64_t num = 2; num < hbound; num++) {
+    if (checkPrime(num)) {
+      sum += num;
+    }
+  }
+  return sum;
 }
 
 uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
-  // вставьте код функции
-  return 1;
+  uint64_t kol = 0;
+  uint64_t pervChisl = 0;
+
+  for(uint64_t num = lbound; num < hbount; num++) {
+    if (checkPrime(num)) {
+      if (pervChisl != 0 && num - pervChisl == 2) {
+        kol++;
+      }
+      pervChisl = num;
+    }
+  }
+  return kol;
 }
