d9f3d2c23bf4d8d63ace1494ce6ebdbb98d95826
nonpluss3d <al.kistanoff2016@yandex.ru>
2025-03-01 12:41:40 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -11,38 +11,22 @@ bool checkPrime(uint64_t value) {
 }
 
 uint64_t nPrime(uint64_t n) {
-  int k = 0, a = 2;
+  uint64_t k = 0, a = 2;
 
   while (k != n) {
-    for (uint64_t i = 2; i <= a / 2; i++) {
-      if (a % i == 0) {
-        k--;
-        break;
-      }
-    }
-    k++;
+    if (checkPrime(a)) k++;
     a++;
   }
   return a - 1;
 }
 
 uint64_t nextPrime(uint64_t value) {
-  uint64_t a = value + 1, flag = 0;
+  uint64_t a = value + 1;
 
-  while (flag == 0) {
-    if (a > 1) {
-      flag = 1;
-      for (uint64_t i = 2; i <= a / 2; i++) {
-        if (a % i == 0) {
-          flag = 0;
-          break;
-        }
-      }
-    }
+  while (!checkPrime(a)) {
     a++;
-
-    if (flag == 1) return a - 1;
   }
+  return a;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
@@ -52,13 +36,7 @@ uint64_t sumPrime(uint64_t hbound) {
     return 0;
   else {
     while (x < hbound) {
-      for (uint64_t i = 2; i <= x / 2; i++) {
-        if (x % i == 0) {
-          sum -= x;
-          break;
-        }
-      }
-      sum += x;
+      if (checkPrime(x)) sum += x;
       x++;
     }
     return sum;
@@ -66,25 +44,10 @@ uint64_t sumPrime(uint64_t hbound) {
 }
 
 uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
-  uint64_t k = 0, flag = 1;
+  uint64_t k = 0;
 
   for (uint64_t i = lbound; i <= hbound - 3; i++) {
-    flag = 1;
-    for (uint64_t j = 2; j <= i / 2; j++) {
-      if (i % j == 0) {
-        flag = 0;
-        break;
-      }
-    }
-    if (flag == 1) {
-      for (uint64_t j = 2; j <= (i + 2) / 2; j++) {
-        if ((i + 2) % j == 0) {
-          flag = 0;
-          break;
-        }
-      }
-    }
-    if (flag == 1) k++;
+    if (checkPrime(i) && checkPrime(i + 2)) k++;
   }
   return k;
 }
