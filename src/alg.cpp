7c0bf9c75e09c519e745ec4764d3caf74766d2c8
nitron1108 <mja29122@gmail.com>
2025-03-01 23:50:25 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -28,31 +28,32 @@ uint64_t nPrime(uint64_t n) {
 }
 
 uint64_t nextPrime(uint64_t value) {
- uint64_t num = value + 1;
-   while (true) {
-        if (checkPrime(num)) {
-            return num;
-        }
-        num++;
-    }
+	uint64_t num = value + 1;
+	while (true) {
+		if (checkPrime(num)) {
+			return num;
+		}
+		num++;
+	}
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-  uint64_t sum = 0;
-    for (uint64_t i = 2; i < hbound; i++) {
-        if (checkPrime(i)) {
-            sum += i;
-        }
-    }
-    return sum;
+	uint64_t sum = 0;
+	for (uint64_t i = 2; i < hbound; i++) {
+		if (checkPrime(i)) {
+			sum += i;
+		}
+	}
+	return sum;
 }
 
 uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
- uint64_t count = 0;
-    for (uint64_t i = lbound; i < hbound; i++) {
-        if (checkPrime(i) && checkPrime(i + 2)) {
-            count++;
-        }
-    }
-    return count;
-}
+	uint64_t count = 0;
+
+	for (uint64_t i = lbound; i < hbound; i++) {
+		if (checkPrime(i) && checkPrime(i + 2)) {
+			count++;
+		}
+	}
+	return count;
+}}
