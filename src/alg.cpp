6adaa6a65516431e0691a5906d098ac719df1734
Eternity8c <morozkind6@gmail.com>
2025-03-01 09:49:51 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,13 +4,13 @@
 #include<cmath>
 
 bool checkPrime(uint64_t value) {
-  if (value < 2) 
+  if (value < 2)
   return false;
-  if (value % 2 == 0 && value > 2) 
+  if (value % 2 == 0 && value > 2)
   return false;
   for (uint64_t i = 3; i <= sqrt(value); i += 2) {
     if (value % i == 0) {
-    return false;
+      return false;
     }
   }
   return true;
@@ -33,16 +33,16 @@ uint64_t nextPrime(uint64_t value) {
   uint64_t candidat = value;
   if (candidat % 2 == 0) {
   candidat++;
-  } else {
-  candidat += 2;
-  }
+    } else {
+      candidat += 2;
+    }
   while (true) {
     if (checkPrime(candidat)) {
-    return candidat;
+        return candidat;
+      }
+      candidat += 2;
     }
-    candidat += 2;
   }
-}
 
 uint64_t sumPrime(uint64_t hbound) {
     uint64_t sum = 0;
@@ -54,16 +54,16 @@ uint64_t sumPrime(uint64_t hbound) {
       }
     }
     return sum;
-}
+  }
 
 uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
-  uint64_t col = 0;
-  uint64_t candidat = lbound;
+    uint64_t col = 0;
+    uint64_t candidat = lbound;
   while (candidat < hbound - 2) {
     if (checkPrime(candidat) && checkPrime(candidat + 2)) {
-      col++;
+        col++;
+      }
+      candidat++;
     }
-    candidat++;
+    return col;
   }
-  return col;
-}
