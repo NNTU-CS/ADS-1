fe910d4baf304b9b6b7fea46f112ab04409e71c4
TechnoMagicc <leyzarof@gmail.com>
2025-03-01 22:04:11 +0300

    added checkPrime and nPrime

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,16 +1,31 @@
-// Copyright 2022 NNTU-CS
+
 #include <cstdint>
 #include "alg.h"
 
 
 bool checkPrime(uint64_t value) {
-  // вставьте код функции
+    if(value < 2){
+        return false;
+    }
+
+    for (uint64_t i = 2; i <= std::sqrt(value); i++)[
+        if (value % i == 0){
+            return false;
+        }
+    ]
   return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-  // вставьте код функции
-  return 2;
+    uint64_t nomer = 0;
+    uint64_t chislo = 2;
+    while (nomer < n){
+        if (checkPrime(chislo)){
+            nomer++;
+        }
+        chislo++;
+    }
+    return chislo - 1;
 }
 
 uint64_t nextPrime(uint64_t value) {
@@ -27,3 +42,4 @@ uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
   // вставьте код функции
   return 1;
 }
+
