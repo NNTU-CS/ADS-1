8a1451c52c2e67be71d79261af99d13a8a9e2e5a
nonpluss3d <al.kistanoff2016@yandex.ru>
2025-03-01 21:09:43 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -23,24 +23,18 @@ uint64_t nPrime(uint64_t n) {
 uint64_t nextPrime(uint64_t value) {
   uint64_t a = value + 1;
 
-  while (!checkPrime(a)) {
-    a++;
-  }
+  while (!checkPrime(a)) a++;
   return a;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-  uint64_t sum = 2, x = 3;
+  uint64_t sum = 0, x = 2;
 
-  if (hbound < 3)
-    return 0;
-  else {
-    while (x < hbound) {
-      if (checkPrime(x)) sum += x;
-      x++;
-    }
-    return sum;
+  while (x < hbound) {
+    if (checkPrime(x)) sum += x;
+    x++;
   }
+  return sum;
 }
 
 uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
