efe8d47002f90d0e5f9528726124b37dceb90299
Nastya3456-fed <Nasty3256@yandex.ru>
2025-03-01 19:40:34 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,4 @@
 // Copyright 2022 NNTU-CS
-#include <cstdint>
-#include "alg.h"
 #include <iostream>
 #include <stdio.h>
 #include <time.h>
@@ -10,28 +8,28 @@
 
 bool checkPrime(uint64_t value) {
     if (value < 2) return false;
-    for (int i = 2; int(i <= pow(value, 0.5)); i++) {
+    for (int i = 2; static_cast<int>(i <= pow(value, 0.5)); i++) {
         if (value % i == 0) {
             return false;
         }
     }
-   return true;
+    return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-   uint64_t k = 0; 
+    uint64_t k = 0;
     uint64_t i = 0;
 
     while (k != n) {
         i += 1;
         bool isPrime = true;
 
-        if (i < 2) continue; 
+        if (i < 2) continue;
 
         for (uint64_t h = 2; h <= sqrt(i); h++) {
             if (i % h == 0) {
                 isPrime = false;
-                break; 
+                break;
             }
         }
 
@@ -60,8 +58,8 @@ uint64_t nextPrime(uint64_t value) {
 }
 
 uint64_t sumPrime(uint64_t hbound) {
- uint64_t sum = 0;
- for (uint64_t present = 2; present < hbound; present++) {
+    uint64_t sum = 0;
+    for (uint64_t present = 2; present < hbound; present++) {
         bool isPrime = true;
         for (uint64_t i = 2; i <= sqrt(present); i++) {
             if (present % i == 0) {
