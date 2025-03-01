71e824f0c7898826f0a9989a2e555c951bc04cfb
Nikolay <99762459+Tidud@users.noreply.github.com>
2025-03-01 16:50:11 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,26 +4,65 @@
 
 
 bool checkPrime(uint64_t value) {
-  // вставьте код функции
+  if (value <= 1 || value % 2 == 0) {
+    return false;
+  }
+  for  (int i = 3; i < sqrt(value) + 1; i += 2) {
+    if (value % i == 0) {
+      return false;
+    }
+  }
   return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-  // вставьте код функции
+  if (n != 1) {
+    uint64_t count = 1, i = 3;
+    while (count != n) {
+      if (checkPrime(n)) {
+        ++count;
+        if (count == n) {
+          return i;
+        }
+        ++i;
+      }
+    }
+  }
   return 2;
 }
 
 uint64_t nextPrime(uint64_t value) {
-  // вставьте код функции
+  if (value != 1) {
+    uint64_t i = value + 1;
+    while (true) {
+      if (checkPrime(i)) {
+        return i;
+      }
+    }
+  }
   return 2;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-  // вставьте код функции
+  if (hbound > 2) {
+    uint64_t sum = 2, i = 3;
+    while (i < hbound) {
+      if (checkPrime(i)) {
+        sum += i;
+      }
+      ++i;
+    }
+    return sum;
+  }
   return 2;
 }
 
 uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
-  // вставьте код функции
-  return 1;
+  uint64_t count = 0;
+  for (int i = lbound; i < hbound - 2; ++i) {
+    if (checkPrime(i) && checkPrime(i + 2)) {
+      count += 1;
+    }
+  }
+  return count;
 }
