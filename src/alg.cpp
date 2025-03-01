85c17ba8f98a2a4269e683e1bc47ef6da167a35c
AnnaShenina <anasenina4@gmail.com>
2025-03-01 16:36:52 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -11,9 +11,9 @@ bool checkPrime(uint64_t value) {
     }
   }
   if (count >= 1)
-    return false;
+  return false;
   else
-    return true;
+  return true;
 }
 
 uint64_t nPrime(uint64_t n) {
@@ -30,34 +30,35 @@ uint64_t nPrime(uint64_t n) {
 
 uint64_t nextPrime(uint64_t value) {
   while (!checkPrime(++value)) {}
-  return value;
-}
+    return value;
+  }
 
 uint64_t sumPrime(uint64_t hbound) {
-  uint64_t sum = 0;
+    uint64_t sum = 0;
   while (--hbound > 1) {
     if (checkPrime(hbound)) {
-      sum += hbound;
+        sum += hbound;
+      }
     }
+    return sum;
   }
-  return sum;
-}
 
 uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
-  uint64_t lastPrime = 0;
-  uint64_t count = 0;
+    uint64_t lastPrime = 0;
+    uint64_t count = 0;
   for (uint64_t i = lbound; i < hbound; i++) {
     if (checkPrime(i)) {
-      lastPrime = i;
-      break;
+        lastPrime = i;
+        break;
+      }
     }
-  }
   for (uint64_t i = lastPrime + 1; i < hbound; i++) {
     if (checkPrime(i)) {
-      if (i - lastPrime == 2)
+      if (i - lastPrime == 2) {
         count++;
         lastPrime = i;
       }
+    }
   }
   return count;
 }
