5e7ff12b7d07974f0b9f65515db2059a60469c2e
Spectxr <leeibov@gmail.com>
2025-03-01 23:21:00 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,26 +5,26 @@
 
 bool checkPrime(uint64_t value) {
   if (value < 2) {
-	  return false;
+    return false;
   }
   for (uint64_t i = 2; i <= sqrt(value); i++) {
-	  if (value % i == 0) {
-		  return false;
-	  }
+    if (value % i == 0) {
+      return false;
+    }
   }
   return true;
 }
 
 uint64_t nPrime(uint64_t n) {
   if (n < 1) {
-	  return 0;
+    return 0;
   }
   uint64_t cnt = 0, num = 1;
   while (cnt < n) {
-	  num++;
-	  if (checkPrime(num) == true) {
-		  cnt++;
-	  }
+    num++;
+    if (checkPrime(num) == true) {
+      cnt++;
+    }
   }
   return num;
 }
@@ -32,7 +32,7 @@ uint64_t nPrime(uint64_t n) {
 uint64_t nextPrime(uint64_t value) {
   uint64_t next = value + 1;
   while (checkPrime(next) == false) {
-	  next++;
+    next++;
   }
   return next;
 }
@@ -40,9 +40,9 @@ uint64_t nextPrime(uint64_t value) {
 uint64_t sumPrime(uint64_t hbound) {
   uint64_t s = 0;
   for (uint64_t i = 0; i < hbound; i++) {
-	  if (checkPrime(i) == true) {
-		  s += i;
-	  }
+    if (checkPrime(i) == true) {
+      s += i;
+    }
   }
   return s;
 }
@@ -50,9 +50,9 @@ uint64_t sumPrime(uint64_t hbound) {
 uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
   uint64_t cnt = 0;
   for (uint64_t i = lbound; i < (hbound - 1); i++) {
-	  if (checkPrime(i) == true && checkPrime(i + 2) == true) {
-		  cnt++;
-	  }
+    if (checkPrime(i) == true && checkPrime(i + 2) == true) {
+      cnt++;
+    }
   }
   return cnt;
 }
