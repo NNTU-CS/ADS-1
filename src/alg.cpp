f67fc8bf30747a82d387853ab81b5f9fbebb5f8e
ratatyq <n.mantorov@yandex.ru>
2025-03-02 21:48:21 +0300

    Update alg3.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,7 +4,7 @@
 
 
 bool checkPrime(uint64_t value) {
-  for (int i = 2; i < value; i++) {
+  for (uint64_t i = 2; i < value; i++) {
     if (value % i == 0) {
       return false;
     }
@@ -14,8 +14,8 @@ bool checkPrime(uint64_t value) {
 
 uint64_t nPrime(uint64_t n) {
   if (n < 1) return 0;
-  int count = 0;
-  int candidate = 1;
+  uint64_t count = 0;
+  uint64_t candidate = 1;
   while (count < n) {
     candidate++;
     if (checkPrime(candidate)) {
@@ -26,7 +26,7 @@ uint64_t nPrime(uint64_t n) {
 }
 
 uint64_t nextPrime(uint64_t value) {
-  int candidate = value + 1;
+  uint64_t candidate = value + 1;
   while (!checkPrime(candidate)) {
     candidate++;
   }
@@ -34,7 +34,7 @@ uint64_t nextPrime(uint64_t value) {
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-  int sum = 0;
+  uint64_t sum = 0;
   for (uint64_t i = 2; i < hbound; i++) {
     if (checkPrime(i)) {
       sum += i;
@@ -44,7 +44,7 @@ uint64_t sumPrime(uint64_t hbound) {
 }
 
 uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
-  int count = 0;
+  uint64_t count = 0;
   for (uint64_t i = lbound; i < hbound - 2; i++) {
     if (checkPrime(i) && checkPrime(i + 2)) {
       count++;
