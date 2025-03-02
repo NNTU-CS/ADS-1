65e1fd9af7010d931b28894c91c0b6b14110aad9
jariuock <garanin.019@gmail.com>
2025-03-02 22:55:10 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,7 +1,7 @@
 // Copyright 2022 NNTU-CS
 #include <cstdint>
 #include <cmath>
-#include "alg.h" 													
+#include "alg.h"
 bool checkPrime(uint64_t value) {
 	for (uint64_t i = 2; i*i<=value; ++i) {
 		if (value % i == 0) {
