5d9bf0a7a1df36676e05d9a59ec5476bcf16ecc2
jariuock <garanin.019@gmail.com>
2025-03-02 20:47:22 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -7,45 +7,45 @@ bool checkPrime(uint64_t value) {
   int count = 1;
   for (int i = 2; i <= sqrt(value); ++i)
   {
-	  if (value % i == 0) 
+	   if (value % i == 0) 
 		  count++;
-	  if (count > 1)
+	   if (count > 1)
 		  return false;
   }
   return true;  
 } 
 
 uint64_t nPrime(uint64_t n) {
-	if (n==1)
+	 if (n==1)
 		return 2;
-	int count = 1;
-	for (int i = 3; count<n; i+=2)
+	 int count = 1;
+	 for (int i = 3; count<n; i+=2)
 	{
-		if (checkPrime(i))
+		  if (checkPrime(i))
 		{
-			++count;
-		}
-		if (count == n)
-			return i; 
-	}
+		   	   ++count;
+		  }
+		  if (count == n)
+			 return i; 
+	 }
 }
 
 uint64_t nextPrime(uint64_t value) {
-	for (int i = value + 1; ; i++)
+	 for (int i = value + 1; ; i++)
 	{
-		if (checkPrime(i))
-			return i;
-	}
+		  if (checkPrime(i))
+			 return i;
+	 }
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-	int sum = 2;
-	for (int i = 3; i < hbound; i += 2)
+	 int sum = 2;
+	 for (int i = 3; i < hbound; i += 2)
 	{
-		if (checkPrime(i))
-			sum += i;
-	}
-	return sum;
+		  if (checkPrime(i))
+			 sum += i;
+	 }
+	 return sum;
 }
 
 
@@ -53,7 +53,7 @@ uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
   int count = 0;
   for (int i = lbound; i <= hbound - 2; ++i)
   {
-	  if (checkPrime(i) && checkPrime(i + 2))
+	   if (checkPrime(i) && checkPrime(i + 2))
 		  ++count;
   }  
   return count;
