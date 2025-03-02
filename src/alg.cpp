bdac3da3bca627af8150580c39edcd047b6fb1d2
Kirill115fz <kirillkuznecov716@gmail.com>
2025-03-02 23:40:04 +0300

    Update_final_5 alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -14,8 +14,9 @@ bool checkPrime(uint64_t value) {
     return false;
   }
   for (uint64_t i = 5; i * i <= value; i = i + 6) {
-    if (value % i == 0 || value % (i + 2) == 0)
+    if (value % i == 0 || value % (i + 2) == 0) {
       return false;
+    }
   }
   return true;
 }
