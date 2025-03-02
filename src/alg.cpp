a2fb4f2e39fb7e41a749e4a1eaf957e835c963b3
AstrakhantsevaNastya <anastasia.astr1904@gmail.com>
2025-03-02 15:17:06 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -7,7 +7,6 @@ bool checkPrime(uint64_t value) {
   if (value <= 1) {
     return false;
   }
-  
   for (uint64_t i = 2; i * i <= value; i++) {
     if (value % i == 0) {
       return false;
@@ -18,25 +17,21 @@ bool checkPrime(uint64_t value) {
 
 uint64_t nPrime(uint64_t n) {
   if (n == 0) {
-      return 0; 
+      return 0;
   }
-  
-  uint64_t count = 0; 
-  uint64_t number = 1; 
-
+  uint64_t count = 0;
+  uint64_t number = 1;
   while (count < n) {
       num++;
       if (checkPrime(number)) {
           count++;
       }
   }
-  return number; 
+  return number;
 }
 
 uint64_t nextPrime(uint64_t value) {
-
   uint64_t nextNum = value + 1;
-
   while (!checkPrime(nextNum)) {
     nextNum++;
   }
@@ -44,9 +39,7 @@ uint64_t nextPrime(uint64_t value) {
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-  
   uint64_t sum = 0;
-
   for (uint64_t j = 2; j < hbound; j++) {
     if (checkPrime(j)) {
       sum += j;
@@ -56,9 +49,7 @@ uint64_t sumPrime(uint64_t hbound) {
 }
 
 uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
-
   uint64_t cnt = 0;
-
   for (uint64_t k = lbound; k < hbound - 2; k++) {
     if (checkPrime(k) && checkPrime(k+2)) {
       cnt++;
