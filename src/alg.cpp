72578d48889d5d189e4b048eea8868f6809f577d
jariuock <garanin.019@gmail.com>
2025-03-02 21:03:26 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,34 +4,33 @@
 
 													
 bool checkPrime(uint64_t value) {
-  int count = 1;
-  for (int i = 2; i <= sqrt(value); ++i)
-  {
-	   if (value % i == 0) 
-		  count++;
-	   if (count > 1)
+  if (value < 2)
+  	return false;
+  for (uint64_t i = 2; i*i <= value; ++i)
+	  {
 		  return false;
-  }
+	  }
   return true;  
 } 
 
 uint64_t nPrime(uint64_t n) {
-	 if (n==1)
-		return 2;
-	 int count = 1;
-	 for (int i = 3; count<n; i+=2)
-	{
-		  if (checkPrime(i))
-		{
-		   	   ++count;
-		  }
-		  if (count == n)
-			 return i; 
-	 }
+	 if (n==0)
+		return 0;
+	 uint64_t count = 0, value = 2;
+	 while (1)
+		 {
+			 if(checkPrime(value))
+			 {
+				 ++count;
+				 if (count == n)
+					 return value;
+			 }
+		 }
+	++value;
 }
 
 uint64_t nextPrime(uint64_t value) {
-	 for (int i = value + 1; ; i++)
+	 for (uint64_t i = value + 1; ; i++)
 	{
 		  if (checkPrime(i))
 			 return i;
@@ -39,8 +38,8 @@ uint64_t nextPrime(uint64_t value) {
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-	 int sum = 2;
-	 for (int i = 3; i < hbound; i += 2)
+	 uint64_t sum = 2;
+	 for (uint64_t i = 3; i < hbound; i += 2)
 	{
 		  if (checkPrime(i))
 			 sum += i;
@@ -50,8 +49,8 @@ uint64_t sumPrime(uint64_t hbound) {
 
 
 uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
-  int count = 0;
-  for (int i = lbound; i <= hbound - 2; ++i)
+  uint64_t count = 0;
+  for (uint64_t i = lbound; i <= hbound - 2; ++i)
   {
 	   if (checkPrime(i) && checkPrime(i + 2))
 		  ++count;
