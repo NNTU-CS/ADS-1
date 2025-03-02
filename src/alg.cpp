5444f91dae8c7f86c99eac2b4d1a7cfe79a7e2ba
TechnoMagicc <leyzarof@gmail.com>
2025-03-02 09:28:58 +0300

    a teper

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,23 +4,23 @@
 
 
 bool checkPrime(uint64_t value) {
-    if(value < 2){
+  if (value < 2) {
         return false;
       }
 
-    for (uint64_t i = 2; i * i <= value; ++i){
+    for (uint64_t i = 2; i * i <= value; ++i) {
          if (value % i == 0) {
             return false;
       }
     }
   return true;
   }
-  
+
 uint64_t nPrime(uint64_t n) {
     uint64_t nomer = 0;
     uint64_t chislo = 2;
-    while (nomer < n){
-        if (checkPrime(chislo)){
+    while (nomer < n) {
+        if (checkPrime(chislo)) {
             nomer++;
         }
       chislo++;
@@ -30,7 +30,7 @@ uint64_t nPrime(uint64_t n) {
   
 uint64_t nextPrime(uint64_t value) {
   uint64_t param = value + 1;
-    while(!checkPrime(param)){
+    while (!checkPrime(param)) {
     param++;
       }
     return param;
@@ -38,8 +38,8 @@ uint64_t nextPrime(uint64_t value) {
   
 uint64_t sumPrime(uint64_t hbound) {
   uint64_t sum = 0;
-    for (int i = 2; i < hbound; i++){
-    if(checkPrime(i)){
+    for (int i = 2; i < hbound; i++) {
+    if (checkPrime(i)) {
       sum += i;
         }
       }
@@ -48,11 +48,10 @@ uint64_t sumPrime(uint64_t hbound) {
   
 uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
   uint64_t count = 0;
-    for (int i = lbound; i < hbound; i++){
-    if (checkPrime(i) && checkPrime(i+2)){
-      count++
+    for (int i = lbound; i < hbound; i++) {
+    if (checkPrime(i) && checkPrime(i+2)) {
+      count++;
         }
       }
     return count;
     }
-  
