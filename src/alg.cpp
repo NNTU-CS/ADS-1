a11128708af5baa2ce9990b9d2d006519b1207be
TechnoMagicc <leyzarof@gmail.com>
2025-03-02 09:37:18 +0300

    (((

--- src/alg.cpp
+++ src/alg.cpp
@@ -15,7 +15,6 @@ bool checkPrime(uint64_t value) {
     }
   return true;
   }
-
 uint64_t nPrime(uint64_t n) {
     uint64_t nomer = 0;
     uint64_t chislo = 2;
@@ -27,7 +26,6 @@ uint64_t nPrime(uint64_t n) {
       }
     return chislo + 1;
 }
-  
 uint64_t nextPrime(uint64_t value) {
   uint64_t param = value + 1;
     while (!checkPrime(param)) {
@@ -35,7 +33,6 @@ uint64_t nextPrime(uint64_t value) {
       }
     return param;
     }
-  
 uint64_t sumPrime(uint64_t hbound) {
   uint64_t sum = 0;
     for (int i = 2; i < hbound; i++) {
@@ -45,7 +42,6 @@ uint64_t sumPrime(uint64_t hbound) {
       }
     return sum;
     }
-  
 uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
   uint64_t count = 0;
     for (int i = lbound; i < hbound; i++) {
