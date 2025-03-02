94ed43d8c19aedebac8bad06c2004288764748ec
eliza3455 <catra7902@gmail.com>
2025-03-02 20:02:11 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,50 +2,64 @@
 #include <cmath>
 #include "alg.h"
 
-
-bool checkPrime(uint64_t value) {
-// вставьте код функции
-  for (uint64_t i = 2; i <= sqrt(value); ++i) {
-    if (value % i == 0) {
+bool isSimple(uint64_t number) {
+  if (number < 2) {
+    return false;
+  }
+  for (uint64_t i = 2; i * i <= number; ++i) {
+    if (number % i == 0) {
       return false;
     }
   }
   return true;
 }
 
-uint64_t nPrime(uint64_t n) {
-// вставьте код функции
-  uint64_t num = 2, count = 0;
-  while (count <= n) {
-    if (checkPrime(num)) {
-      count++;
-    }
-    if (count == n) {
-      break;
-    } else {
-      num++;
-    }
+uint64_t getNthSimple(uint64_t index) {
+  if (index == 0) {
+    return 0;
   }
-  return num;
+  uint64_t num = 2, count = 0;
+  while (true) {
+    if (isSimple(num)) {
+      count++;
+      if (count == index) {
+        return num;
+      }
+    }
+    num++;
+  }
 }
 
-uint64_t nextPrime(uint64_t value) {
-// вставьте код функции
-  uint64_t num = value + 1;
-  while (!checkPrime(num)) {
+uint64_t findNextSimple(uint64_t number) {
+  uint64_t num = number + 1;
+  while (!isSimple(num)) {
     num++;
   }
   return num;
 }
 
-uint64_t sumPrime(uint64_t hbound) {
-// вставьте код функции
-  uint64_t num = 2, sum = 0;
-  while (num < hbound) {
-    if (checkPrime(num)) {
+uint64_t sumSimples(uint64_t limit) {
+  if (limit < 2) {
+    return 0;
+  }
+  uint64_t sum = 0;
+  for (uint64_t num = 2; num < limit; ++num) {
+    if (isSimple(num)) {
       sum += num;
     }
-    num++;
   }
   return sum;
+}
+
+uint64_t twinPrimes(uint64_t low, uint64_t high) {
+  if (high < 3 || low > high) {
+    return 0;
+  }
+  uint64_t twinPairs = 0;
+  for (uint64_t num = low; num + 2 < high; ++num) {
+    if (isSimple(num) && isSimple(num + 2)) {
+      ++twinPairs;
+    }
+  }
+  return twinPairs;
 }
