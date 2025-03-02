151b17e615e02c007ec34496682ade225fc94a08
Tripleez <art_khorosh@vk.com>
2025-03-02 22:26:11 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -9,8 +9,8 @@ bool checkPrime(uint64_t value) {
   }
   for(int i = 2; i < value; i++){
       if (value % i == 0){
-          return false;
-      }
+  return false;
+  }
   }
   return true;
 }
@@ -19,10 +19,10 @@ uint64_t nPrime(uint64_t n) {
   uint64_t c = 0;
   uint64_t num = 1;
   while (c < n){
-      num +=1;
+  num +=1;
       if (checkPrime(num)){
-          c += 1;
-      }
+  c += 1;
+  }
   }
   return num;
 }
@@ -31,9 +31,9 @@ uint64_t nextPrime(uint64_t value) {
   uint64_t c = value + 1;
   while(1){
       if(checkPrime(c)){
-          return c;
-      }
-      c +=1;
+  return c;
+  }
+  c +=1;
   }
   return 2;
 }
@@ -42,8 +42,8 @@ uint64_t sumPrime(uint64_t hbound) {
   uint64_t sum = 0;
   for(int i  = 2; i < hbound; i++){
       if(checkPrime(i)){
-          sum += i;
-      }
+  sum += i;
+  }
   }
   return sum;
 }
@@ -53,9 +53,9 @@ uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
   for(uint64_t  i = lbound; i < hbound - 2; i++){
       if(checkPrime(i)){
           if(checkPrime(i + 2)){
-              c += 1;
-          }
-      }
+  c += 1;
+  }
+  }
   }
   
   return c;
