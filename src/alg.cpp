44b8de9320217111560b303785f1fce9dfa9e96a
TechnoMagicc <leyzarof@gmail.com>
2025-03-02 08:31:09 +0300

    added primeSum

--- src/alg.cpp
+++ src/alg.cpp
@@ -31,17 +31,22 @@ uint64_t nPrime(uint64_t n) {
 uint64_t nextPrime(uint64_t value) {
   uint64_t param = value + 1;
   while(!checkPrime(param)){
-    param++
+    param++;
   }
   return param;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-  // вставьте код функции
-  return 2;
+  uint64_t sum = 0;
+  for (int i = 2; i < hbound; i++){
+    if(checkPrime(i)){
+      sum += i;
+    }
+  }
+  return sum;
 }
 
 uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
-  // вставьте код функции
+  
   return 1;
 }
