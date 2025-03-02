c8a40a2f6c1d2f72e7115ae5ddfaffd4ddafb8f7
YaroslavChinyakov <chinyakov777@gmail.com>
2025-03-02 23:50:54 +0300

    4th alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,56 +1,57 @@
+//Copyright 2022 <Copyright owner>
 #include <cstdint>
 #include "alg.h"
 bool checkPrime(uint64_t value) {
-  if (value < 2) 
-    return false;
+    if (value < 2) 
+  return false;
     for (uint64_t i = 2; i * i <= value; ++i) {
-if (value % i == 0) return false;
-}
-return true;
+    if (value % i == 0) return false;
+  }
+  return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-  if (n == 0) return 0; 
-uint64_t count = 0;
-uint64_t num = 1;
+  if (n == 0) return 0;
+  uint64_t count = 0;
+  uint64_t num = 1;
     while (count < n) {
-++num;
+    ++num;
         if (checkPrime(num)) {
-++count;
-}
-}
-return num;
+      ++count;
+    }
+  }
+  return num;
 }
 
 uint64_t nextPrime(uint64_t value) {
   uint64_t a = value + 1;
     while (!checkPrime(a)) {
-++a;
-}
-return a;
+    ++a;
+  }
+  return a;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
   uint64_t sum = 0;
     for (uint64_t i = 2; i < hbound; ++i) {
         if (checkPrime(i)) {
-sum += i;
-}
-}
-return sum;
+      sum += i;
+    }
+  }
+  return sum;
 }
 
 uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
   uint64_t k = 0;
-uint64_t prevPrime = 0;
+  uint64_t prevPrime = 0;
   
     for (uint64_t i = lbound; i < hbound; ++i) {
         if (checkPrime(i)) {
             if (prevPrime != 0 && i - prevPrime == 2) {
-++k;
-}
-prevPrime = i;
-}
-}
-return k;
+        ++k;
+      }
+      prevPrime = i;
+    }
+  }
+  return k;
 }
