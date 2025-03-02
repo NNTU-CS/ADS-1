b536d1b1e52d3e1b324bf2344bd2b4d2a3896ef1
Kirill115fz <kirillkuznecov716@gmail.com>
2025-03-02 19:36:28 +0300

    впроь

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,9 +4,9 @@
 
 // Функция проверки, является ли число простым. Изменен алгоритм немного.
 bool checkPrime(uint64_t value) {
- if (value <= 1) return false;
+  if (value <= 1) return false;
  if (value <= 3) return true;
- if (value % 2 == 0 || value % 3 == 0) return false;
+  if (value % 2 == 0 || value % 3 == 0) return false;
     for (uint64_t i = 5; i * i <= value; i = i + 6) {
         if (value % i == 0 || value % (i + 2) == 0)
             return false;
