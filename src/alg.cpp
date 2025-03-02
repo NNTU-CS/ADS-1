7e3448a9a1ddaa9914390dd2abcfc52ea6436940
kate7-2 <katemate484@gmail.com>
2025-03-02 22:42:09 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -8,7 +8,7 @@ bool checkPrime(uint64_t value) {
   if (value == 2) return true;
   if (value % 2 == 0) return false;
   for (uint64_t i = 3; i * i <= value; i += 2) {
-      if (value % i == 0) return false;
+  if (value % i == 0) return false;
   }
   return true;
 }
@@ -22,9 +22,9 @@ uint64_t nPrime(uint64_t n) {
   while (k < n) {
     num += 2; 
     if (checkPrime(num)) {
-        k++;
+    k++;
     }
-}
+  }
   return num;
 }
 
@@ -33,29 +33,26 @@ uint64_t nextPrime(uint64_t value) {
   if (value <= 1) return 2;  
   uint64_t num;
   if (value % 2 == 0) {
-      num = value + 1;
+  num = value + 1;
   }
   else {
-      num = value + 2;
+  num = value + 2;
   }
   while (!checkPrime(num)) {
-      num += 2;
+  num += 2;
   }
 
   return num;
 }
   
-
-
-
 uint64_t sumPrime(uint64_t hbound) {
   if (hbound <= 2) return 0;
   uint64_t sum = 0;
 
   for (uint64_t k = 2; k < hbound; k++) {
       if (checkPrime(k)) {
-          sum += k;
-      }
+      sum += k;
+  }
   }
   return sum;
 }
@@ -65,19 +62,19 @@ uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
   if (hbound < 4) return 0;
   uint64_t k = 0;
   if (lbound <= 2 && hbound > 3 ) {
-      k++;
-   }
+  k++;
+  }
 
   uint64_t nch = lbound;
   if (nch % 2 == 0) {
-      nch += 1;
-   }
+  nch += 1;
+  }
   uint64_t j = nch;
   while (j + 2 < hbound) {
       if (checkPrime(j) && checkPrime(j + 2)) {
-          k++;
-      }
-      j += 2;
+      k++;
+  }
+  j += 2;
   }
 
   return k;
