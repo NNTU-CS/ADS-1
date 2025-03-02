9538c82b1b6576475bf8c4156851defb882a715c
Egor Mitrofanov <82968933+MaybeeYou@users.noreply.github.com>
2025-03-02 14:04:17 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -22,25 +22,60 @@ bool checkPrime(uint64_t value) {
     }
   }
   return true;
-  return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-  // вставьте код функции
-  return 2;
+  uint64_t var = 2;
+  int res = 0;
+  while (true) {
+    if (checkPrime(var)) {
+      res += 1;
+    }
+    if (res == n) {
+      return var;
+    }
+    var += 1;
+  }
 }
 
 uint64_t nextPrime(uint64_t value) {
-  // вставьте код функции
-  return 2;
+  value++;
+
+  if (value > 2 && value % 2 == 0) {
+    value++;
+  }
+
+  while (true) {
+    if (checkPrime(value)) {
+      return value;
+    }
+    value++;
+  }
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-  // вставьте код функции
-  return 2;
+  uint64_t sum = 0;
+
+  if (hbound > 2) {
+    sum += 2;
+  }
+
+  for (uint64_t i = 3; i < hbound; i+=2) {
+    if (checkPrime(i)) {
+      sum += i;
+    }
+  }
+  return sum;
 }
 
 uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
-  // вставьте код функции
-  return 1;
+  uint64_t count = 0; // Счетчик пар близнецов
+
+  for (uint64_t i = lbound; i < hbound - 2; ++i) {
+    if (checkPrime(i) && checkPrime(i + 2)) {
+      ++count;
+    }
+  }
+
+  return count;
 }
