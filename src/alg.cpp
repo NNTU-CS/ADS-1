bb925ba516dde6531d4bac03803f70b410ed166d
TechnoMagicc <leyzarof@gmail.com>
2025-03-02 08:39:13 +0300

    done

--- src/alg.cpp
+++ src/alg.cpp
@@ -47,6 +47,11 @@ uint64_t sumPrime(uint64_t hbound) {
 }
 
 uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
-  
-  return 1;
+  uint64_t count = 0;
+  for (int i = lbound; i < hbound; i++){
+    if (checkPrime(i) && checkPrime(i+2)){
+      count++
+    }
+  }
+  return count;
 }
