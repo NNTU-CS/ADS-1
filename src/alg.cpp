40f1a0f635ec111f2d4677cf9c7b88a864935c4d
Tripleez <art_khorosh@vk.com>
2025-03-02 22:49:09 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,13 +4,13 @@
 
 
 bool checkPrime(uint64_t value) {
-  if(value < 2){
+  if (value < 2) {
     return false;
   }
-  for(int i = 2; i < value; i++){
-      if (value % i == 0){
-  return false;
-  }
+  for (int i = 2; i < value; i++) {
+      if (value % i == 0) {
+      return false;
+    }
   }
   return true;
 }
@@ -18,11 +18,11 @@ bool checkPrime(uint64_t value) {
 uint64_t nPrime(uint64_t n) {
   uint64_t c = 0;
   uint64_t num = 1;
-  while (c < n){
-  num +=1;
-      if (checkPrime(num)){
-  c += 1;
-  }
+  while (c < n) {
+    num +=1;
+      if (checkPrime(num)) {
+      c += 1;
+    }
   }
   return num;
 }
@@ -30,33 +30,32 @@ uint64_t nPrime(uint64_t n) {
 uint64_t nextPrime(uint64_t value) {
   uint64_t c = value + 1;
   while(1){
-      if(checkPrime(c)){
-  return c;
-  }
-  c +=1;
+      if (checkPrime(c)) {
+      return c;
+    }
+    c +=1;
   }
   return 2;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
   uint64_t sum = 0;
-  for(int i  = 2; i < hbound; i++){
-      if(checkPrime(i)){
-  sum += i;
-  }
+  for (int i  = 2; i < hbound; i++) {
+      if (checkPrime(i)) {
+      sum += i;
+    }
   }
   return sum;
 }
 
 uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
   int c = 0;
-  for(uint64_t  i = lbound; i < hbound - 2; i++){
-      if(checkPrime(i)){
-          if(checkPrime(i + 2)){
-  c += 1;
+  for (uint64_t  i = lbound; i < hbound - 2; i++) {
+      if (checkPrime(i)) {
+          if (checkPrime(i + 2)) {
+        c += 1;
+      }
+    }
   }
-  }
-  }
-  
   return c;
 }
