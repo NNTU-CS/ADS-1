c65bb0290014a38cd9174818dda76f65a6095cf0
Mariya-Norkina <128129585+Mariya-Norkina@users.noreply.github.com>
2025-03-03 01:08:36 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -12,15 +12,15 @@ bool checkPrime(uint64_t value) {
   return true;
 }
 uint64_t nPrime(uint64_t n) {
-  uint64_t cnt = 0;
-  uint64_t num = 1;
-  while (cnt < n) {
-    num++;
-    if (checkPrime(cnt)) {
-      num++;
-    }
-  }
-  return cnt;
+ uint64_t cnt = 0;
+ uint64_t num = 2;
+ while (true) {
+     if (checkPrime(num)) {
+         cnt++;
+         if (cnt == n) return num;
+     }
+     num++;
+ }
 }
 uint64_t nextPrime(uint64_t value) {
   uint64_t num = value + 1;
