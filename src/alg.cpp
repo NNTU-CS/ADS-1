ff93c86458ab9341e36c49f1ce4143e4d56001bb
HntgerL <lbedevd@gmail.com>
2025-03-02 14:58:45 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,15 +4,12 @@
 
 
 bool checkPrime(uint64_t value) {
-  for (uint64_t i = 2; i <= (value / 2); i++)
-{
-    if (value%i==0)
-{
+  for (uint64_t i = 2; i <= (value / 2); i++){
+    if (value%i == 0){
       return false;
     }
   }
-  if (value==0 || value ==1)
-{
+  if (value == 0 || value == 1){
     return false;
   }
   return true;
@@ -20,11 +17,9 @@ bool checkPrime(uint64_t value) {
 
 uint64_t nPrime(uint64_t n) {
   uint64_t value = 0;
-  for (uint64_t i = 0; i < n; ++i)
-{
+  for (uint64_t i = 0; i < n; ++i){
     value += 1;
-    while (checkPrime(value) != true)
-{
+    while (checkPrime(value) != true){
       value += 1;
     }
   }
@@ -33,34 +28,26 @@ uint64_t nPrime(uint64_t n) {
 
 uint64_t nextPrime(uint64_t value) {
   value += 1;
-  while (checkPrime(value) != true)
-{
+  while (checkPrime(value) != true){
     value += 1;
   }
   return value;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-  uint64_t value = 0;
   uint64_t sum = 0;
-  for (uint64_t i = 0; i < (hbound-1); ++i)
-{
-    value += 1;
-    while (checkPrime(value) != true)
-{
-      value += 1;
+  for (uint64_t value = 0; value < hbound-1; ++value){
+    if (checkPrime(value) == true){
+      sum += value;
     }
-    sum += value;
   }
   return sum;
-  }
+}
 
 uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
   uint64_t count = 0;
-  for (uint64_t i = lbound; i < (hbound-1); ++i)
-{
-    if (checkPrime(i) == true && checkPrime(i + 2) == true && i+2<hbound)
-{
+  for (uint64_t i = lbound; i < (hbound-1); ++i){
+    if (checkPrime(i) == true && checkPrime(i + 2) == true && i+2 < hbound){
       count += 1;
     }
   }
