72ae8a146e5d74735a4557e391601972b01053d1
YaroslavChinyakov <chinyakov777@gmail.com>
2025-03-02 23:22:44 +0300

    3rd alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,8 @@
-// Copyright 2022 NNTU-CS
 #include <cstdint>
 #include "alg.h"
-
-
 bool checkPrime(uint64_t value) {
-  if (value < 2) return false;
+  if (value < 2) 
+    return false;
     for (uint64_t i = 2; i * i <= value; ++i) {
 if (value % i == 0) return false;
 }
