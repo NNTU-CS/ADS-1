d2eebe53b88d6da55ed3ad4f19649792fe9b379c
eliza3455 <catra7902@gmail.com>
2025-03-02 20:16:41 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -21,7 +21,7 @@ uint64_t nPrime(uint64_t index) {
   }
   uint64_t num = 2, count = 0;
   while (true) {
-    if (isSimple(num)) {
+    if (checkPrime(num)) {
       count++;
       if (count == index) {
         return num;
@@ -33,7 +33,7 @@ uint64_t nPrime(uint64_t index) {
 
 uint64_t nextPrime(uint64_t number) {
   uint64_t num = number + 1;
-  while (!isSimple(num)) {
+  while (!checkPrime(num)) {
     num++;
   }
   return num;
@@ -45,7 +45,7 @@ uint64_t sumPrime(uint64_t limit) {
   }
   uint64_t sum = 0;
   for (uint64_t num = 2; num < limit; ++num) {
-    if (isSimple(num)) {
+    if (checkPrime(num)) {
       sum += num;
     }
   }
@@ -58,7 +58,7 @@ uint64_t twinPrimes(uint64_t low, uint64_t high) {
   }
   uint64_t twinPairs = 0;
   for (uint64_t num = low; num + 2 < high; ++num) {
-    if (isSimple(num) && isSimple(num + 2)) {
+    if (checkPrime(num) && checkPrime(num + 2)) {
       ++twinPairs;
     }
   }
