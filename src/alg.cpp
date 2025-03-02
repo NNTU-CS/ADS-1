9ff4825e78402c5155963d4547cfd7c2ddbeaf9b
Ainz-Ooal-Goung <vadimdfl129@gmail.com>
2025-03-02 20:04:11 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,36 +1,44 @@
 // Copyright 2022 NNTU-CS
 #include <cstdint>
+#include <cmath>
 #include "alg.h"
 
-
 bool checkPrime(uint64_t value) {
-  if (value < 2) return false;
+    if (value < 2) {
+        return false;
+    }
     for (uint64_t i = 2; i * i <= value; ++i) {
-        if (value % i == 0)
+        if (value % i == 0) {
             return false;
-  return true;
+        }
+    }
+    return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-  uint64_t count = 0, num = 1;
+    uint64_t count = 0, num = 1;
     while (count < n) {
         ++num;
-        if (checkPrime(num))
+        if (checkPrime(num)) {
             ++count;
+        }
     }
     return num;
 }
 
 uint64_t nextPrime(uint64_t value) {
-    while (!checkPrime(++value));
+    do {
+        ++value;
+    } while (!checkPrime(value));
     return value;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
     uint64_t sum = 0;
     for (uint64_t i = 2; i < hbound; ++i) {
-        if (checkPrime(i))
+        if (checkPrime(i)) {
             sum += i;
+        }
     }
     return sum;
 }
@@ -39,8 +47,9 @@ uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
     uint64_t count = 0, prev = 0;
     for (uint64_t i = lbound; i < hbound; ++i) {
         if (checkPrime(i)) {
-            if (prev && i - prev == 2)
+            if (prev && i - prev == 2) {
                 ++count;
+            }
             prev = i;
         }
     }
