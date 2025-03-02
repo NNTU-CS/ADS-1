6a6e90ced7a61a459d040ad48ce0186b66626688
swagjjs <liazemcova00@gmail.com>
2025-03-02 16:10:13 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,7 +4,15 @@
 
 
 bool checkPrime(uint64_t value) {
-  // вставьте код функции
+  if (value % 2 == 0) {
+        return false;
+    }
+
+  for (int i = 3; i < sqrt(value); i += 2) {
+      if (value % i == 0) {
+          return false;
+      }
+  }
   return true;
 }
 
