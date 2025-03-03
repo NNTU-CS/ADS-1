24ced276bd8d772c1ad5d032b4695e4af970e62f
netherNet <whit3ufo@gmail.com>
2025-03-03 21:08:14 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,26 +4,52 @@
 
 
 bool checkPrime(uint64_t value) {
-  // вставьте код функции
-  return true;
+ if (value == 1)
+	return false;
+
+for (uint64_t i = 2; i*i <= value; i++) {
+	if (value % i == 0)
+		return false;
+}
+
+return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-  // вставьте код функции
-  return 2;
+ uint64_t count = 0;
+
+for (int i = 2;;i++) {
+
+	if(checkPrime(i))
+		++count;
+
+	if(count == n)
+		return i;
+}
 }
 
 uint64_t nextPrime(uint64_t value) {
-  // вставьте код функции
-  return 2;
+  for (int i = value + 1;; i++)
+	if (checkPrime(i))
+		return i;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-  // вставьте код функции
-  return 2;
+ uint64_t sum = 0;
+
+for (int i = 1; i < hbound; i++) {
+	if (checkPrime(i))
+		sum += i;
+}
+return sum;
 }
 
 uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
-  // вставьте код функции
-  return 1;
+  uint64_t sum = 0;
+
+for (int i = lbound; i < hbound - 2; i++) {
+	if (checkPrime(i) && checkPrime(i + 2))
+		sum++;
+}
+return sum;
 }
