16004a588fecf5620b8d322e31b493e050c737c8
AnastasiaMozzhalova <mazhe.nasty@gmail.com>
2025-03-03 13:46:51 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,11 +4,13 @@
 #include "alg.h"
 
 bool checkPrime(uint64_t value) {
-  if (value < 2) return false;
-  if (value == 2) return true;
-  if (value % 2 == 0) return false;
-  for (uint64_t i = 3; i <= value*value; i += 2) {
-    if (value % i == 0) return false;
+  if (value <= 1) return false;
+  if (value <= 3) return true;
+  if (value % 2 == 0 || value % 3 == 0) return false;
+  for (uint64_t i = 5; i * i <= value; i += 6) {
+    if (value % i == 0 || value % (i + 2) == 0) {
+      return false;
+    }
   }
   return true;
 }
