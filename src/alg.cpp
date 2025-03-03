b187fff8658396bf7a77af5a721d3ab755a5260a
AnastasiaMozzhalova <mazhe.nasty@gmail.com>
2025-03-03 14:30:30 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -45,7 +45,7 @@ uint64_t nextPrime(uint64_t value) {
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-  uint64_t sum;
+  uint64_t sum=0;
   for (uint64_t i = 2; i < hbound; ++i) {
     if (checkPrime(i)) {
       sum += i;
@@ -55,7 +55,7 @@ uint64_t sumPrime(uint64_t hbound) {
 }
 
 uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
-  uint64_t count;
+  uint64_t count=0;
   std::vector<uint64_t> primes;
 
   for (uint64_t i = lbound; i < hbound; ++i) {
