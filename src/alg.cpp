ea1f0638efc00cb65c653145891b58888f890ae3
OlgaTimokhina <olya.tim0khina11@gmail.com>
2025-03-05 01:19:14 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,21 +4,22 @@
 
 
 bool checkPrime(uint64_t value) {
-  if (value <= 1) return false;
-    if (value == 2) return true;
-    if (value % 2 == 0) return false;
-    for (uint64_t i = 3; i < sqrt(value); i++)
-        if (value % i == 0) return false; 
-    return true;
+  if (value < 2) return false;
+  for (uint64_t i = 2; i * i <= value; i++) {
+    if (value % i == 0) return false;
+  }
+  return true;
 }
 
 uint64_t nPrime(uint64_t n) {
   uint64_t num = 2;
-    uint64_t count = 0;
+  uint64_t count = 0;
     while (true) {
-        if (checkPrime(num)) count += 1;
+      if (checkPrime(num)) {
+        count++;
         if (count == n) return num;
-        num += 1;
+      }
+      num++;
     }
 }
 
@@ -39,10 +40,11 @@ uint64_t sumPrime(uint64_t hbound) {
 
 uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
   uint64_t count = 0;
-    for (uint64_t i = lbound; i < hbound-2; i++) {
-        if (checkPrime(i) && checkPrime(i + 2)) {
-            count += 1;
-        }
+  if (lbound >= hbound) return 0;
+  for (uint64_t i = lbound; i < hbound; i++) {
+    if (checkPrime(i) && checkPrime(i + 2)) {
+      count++;
     }
-    return count;
+  }
+  return count;
 }
