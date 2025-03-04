8cabfe2d84fa62c28673c2d69d8d304c3b358ae2
OlgaTimokhina <olya.tim0khina11@gmail.com>
2025-03-05 00:51:13 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,26 +4,45 @@
 
 
 bool checkPrime(uint64_t value) {
-  // вставьте код функции
-  return true;
+  if (value <= 1) return false;
+    if (value == 2) return true;
+    if (value % 2 == 0) return false;
+    for (uint64_t i = 2; i < sqrt(value); i++)
+        if (value % i == 0) return false; 
+    return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-  // вставьте код функции
-  return 2;
+  uint64_t num = 2;
+    uint64_t count = 0;
+    while (true) {
+        if (checkPrime(num)) count += 1;
+        if (count == n) return num;
+        num += 1;
+    }
 }
 
 uint64_t nextPrime(uint64_t value) {
-  // вставьте код функции
-  return 2;
+  while (true) {
+        value++;
+        if (checkPrime(value)) return value;
+    }
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-  // вставьте код функции
-  return 2;
+  uint64_t sum = 0;
+    for (uint64_t i = 2; i < hbound; ++i) {
+        if (checkPrime(i)) sum += i;
+    }
+    return sum;
 }
 
 uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
-  // вставьте код функции
-  return 1;
+  uint64_t count = 0;
+    for (uint64_t i = lbound; i < hbound-2; i++) {
+        if (checkPrime(i) && checkPrime(i + 2)) {
+            count += 1;
+        }
+    }
+    return count;
 }
