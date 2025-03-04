88d5b700059e3f0ca84f7532091e2df47ad68d29
tilt377-3 <wardroiddarkness@gmail.com>
2025-03-04 19:11:47 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,28 +2,66 @@
 #include <cstdint>
 #include "alg.h"
 
-
 bool checkPrime(uint64_t value) {
-  // вставьте код функции
+  if (value <= 1) {
+    return false;
+  }
+  if (value == 2) {
+    return true;
+  }
+  if (value % 2 == 0) {
+    return false;
+  }
+  for (int i = 3; i * i <= value; i += 2) {
+    if (value % i == 0) {
+      return false;
+    }
+  }
   return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-  // вставьте код функции
-  return 2;
+  int final = 2;
+  int count = 0;
+  while (1) {
+    if (checkPrime(final)) {
+      count += 1;
+    }
+    if (count == n) {
+      return final;
+    }
+    final += 1;
+  }
 }
 
 uint64_t nextPrime(uint64_t value) {
-  // вставьте код функции
-  return 2;
+  while (1) {
+    value++;
+    if (checkPrime(value)) {
+      return value;
+    }
+  }
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-  // вставьте код функции
-  return 2;
+  int sum = 0;
+  if (hbound > 2) {
+    sum += 2;
+  }
+  for (int i = 3; i < hbound; i += 2) {
+    if (checkPrime(i)) {
+      sum += i;
+    }
+  }
+  return sum;
 }
 
 uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
-  // вставьте код функции
-  return 1;
+  int count = 0;
+  for (lbound; lbound < hbound-2; lbound += 1) {
+    if (checkPrime(lbound) and checkPrime(lbound + 2)) {
+      count += 1;
+    }
+  }
+  return count;
 }
