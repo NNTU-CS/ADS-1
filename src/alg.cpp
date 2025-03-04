0406fe3425b833455067093d1c1cfc2d0d9cc254
Br0vnBear <freezerleonid@gmail.com>
2025-03-04 18:12:04 +0300

    alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,69 +4,62 @@
 #include <cmath>
 
 bool checkPrime(uint64_t value) {
-    int k = 0;
-    for (int i = 1; i <= sqrt(value); i++) {
-        if (value % i == 0)
-            k += 1;
-    }
-    if (k == 1)
-        return true;
-    else
-        return false;
+  uint64_t k;
+  for (int i = 1; i <= sqrt(value); i++) {
+    if (value % i == 0)
+      k += 1;
+  }
+  if (k == 1)
+    return true;
+  else
+    return false;
 }
 
 uint64_t nPrime(uint64_t n) {
-    int i = 0, a = 2, k = 0;
-    while (i < n) {
-        k = 0;
-        for (int j = 1; j <= sqrt(a); j++) {
-            if (a % j == 0)
-                k += 1;
-        }
-        if (k == 1)
-            i += 1;
-        a += 1;
+  uint64_t i, a = 2, k;
+  while (i < n) {
+    k = 0;
+    for (int j = 1; j <= sqrt(a); j++) {
+      if (a % j == 0)
+        k += 1;
     }
-    return a - 1;
+    if (k == 1)
+      i += 1;
+    a += 1;
+  }
+  return a - 1;
 }
 
-
 uint64_t nextPrime(uint64_t value) {
-    int i = 0, k = 0;
-    while (k != 1) {
-        k = 0;
-        value += 1;
-        for (int j = 1; j <= sqrt(value); j++) {
-            if (value % j == 0)
-                k += 1;
-        }
+  uint64_t i, k;
+  while (k != 1) {
+    k = 0;
+    value += 1;
+    for (int j = 1; j <= sqrt(value); j++) {
+      if (value % j == 0)
+        k += 1;
     }
-    return value;
+  }
+  return value;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-    int i = 1, k = 0, sum = 0;
-    while (i < hbound) {
-        k = 0;
-        for (int j = 1; j <= sqrt(i); j++) {
-            if (i % j == 0)
-                k += 1;
-        }
-        if (k == 1) {
-            sum += i;
-        }
-        i += 1;
+  uint64_t i = 2, sum;
+  while (i < hbound) {
+    if (checkPrime(i)) {
+      sum += i;
     }
-    return sum - 1;
+  }
+  return sum - 1;
 }
 
 uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
-    int k = 0, a = lbound;
-    while (a + 2 < hbound) {
-        if (checkPrime(a) && checkPrime(a + 2)) {
-            k += 1;
-        }
-        a += 1;
+  uint64_t k, a = lbound;
+  while (a + 2 < hbound) {
+    if (checkPrime(a) && checkPrime(a + 2)) {
+      k += 1;
     }
-    return k;
+    a += 1;
+  }
+  return k;
 }
