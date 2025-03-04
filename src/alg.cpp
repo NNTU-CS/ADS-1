c8c2639cfafd1f4992ef74a1bea66af8659b47a0
notpsnk <frazzle.kibbles_0i@icloud.com>
2025-03-04 11:45:49 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -9,7 +9,7 @@
 bool checkPrime(uint64_t value) {
   if (value < 2) return false;
   if (value == 2 || value == 3) return true;
-  if (value % 2 == 0 || value % 3 == 0) return (value == 2 || value == 3);
+  if (value % 2 == 0 || value % 3 == 0) return true;
   uint64_t limit = static_cast<uint64_t>(std::sqrt(value));
   for (uint64_t i = 5; i <= limit; i += 6) {
     if (value % i == 0 || value % (i + 2) == 0) {
