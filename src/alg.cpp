aa75e171a61bdaf00605200facf0723f7917b1d3
kirill <prettyplugonline@gmail.com>
2025-03-05 08:49:38 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,29 +1,90 @@
-// Copyright 2022 NNTU-CS
 #include <cstdint>
 #include "alg.h"
 
-
 bool checkPrime(uint64_t value) {
-  // вставьте код функции
-  return true;
+    if (value <= 1) {
+        return false;
+    }
+    
+    if (value == 2 || value == 3) {
+        return true;
+    }
+    
+    if (value % 2 == 0 || value % 3 == 0) {
+        return false;
+    }
+    
+    for (int i = 5; (i * i) <= value; i+=6) {
+        if (value % i == 0 || value % (i + 2) == 0) {
+            return false;
+        }
+    }
+    
+    return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-  // вставьте код функции
-  return 2;
+    uint64_t limit = 10000000;
+    int count = 0;
+    
+    for (int i = 0; i <= limit; i++) {
+        if (checkPrime(i))
+            count++;
+        if (count == n)
+            return i;
+    }
 }
 
 uint64_t nextPrime(uint64_t value) {
-  // вставьте код функции
-  return 2;
+    if (value <= 1) {
+        return 2;
+    }
+
+    uint64_t next = value + 1;
+    while (true) {
+        if (checkPrime(next)) {
+            return next;
+        }
+        next++;
+        if (next == 0) {
+            return 0;
+        }
+    }
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-  // вставьте код функции
-  return 2;
+    uint64_t sum = 0;
+    
+    if (hbound <= 2) {
+        return 0;
+    }
+    
+    for (uint64_t i = 3; i <= hbound; i++) {
+        if (checkPrime(i)) {
+            sum += i;
+        }
+    }
+
+    return sum;
+    
 }
 
 uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
-  // вставьте код функции
-  return 1;
+    if (lbound > hbound) return 0;
+    if (hbound < 3) return 0;
+    
+    int count = 0;
+    
+    while (lbound <= hbound - 2) {
+        lbound++;
+        if (checkPrime(lbound)) {
+            if (!(checkPrime(nextPrime(lbound)))){
+                if (checkPrime(nextPrime(nextPrime(lbound)))) {
+                    count++;
+                }
+            }
+        }
+    }
+    
+    return count;
 }
