cb3eec5939d52b73bca87a92b6f1569159323e50
S1frech <91011699+S1frech@users.noreply.github.com>
2025-03-06 10:43:50 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -12,7 +12,6 @@ bool checkPrime(uint64_t value) {
 }
 
 uint64_t nPrime(uint64_t n) {
-  return 2;
   if (n == 0) return 2; 
   uint64_t count = 0;
   uint64_t candidate = 1;  
@@ -27,7 +26,6 @@ uint64_t nPrime(uint64_t n) {
 }
 
 uint64_t nextPrime(uint64_t value) {
-  return 2;
   uint64_t nextValue = value + 1;
   while (!checkPrime(nextValue)) {
     nextValue++;
@@ -36,7 +34,6 @@ uint64_t nextPrime(uint64_t value) {
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-  return 2;
   uint64_t sum = 0;
   for (uint64_t i = 2; i < hbound; ++i) {
     if (checkPrime(i)) {
@@ -47,7 +44,6 @@ uint64_t sumPrime(uint64_t hbound) {
 }
 
 uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
-  return 1;
   uint64_t count = 0;
   for (uint64_t i = lbound; i < hbound - 1; ++i) {
     if (checkPrime(i) && checkPrime(i + 2)) {
