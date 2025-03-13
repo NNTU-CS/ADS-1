2ab2a6fdcc517349eb2de15206ea8e7edc65034f
kudimovkKosta-24-ist-3 <kudimov900459@yandex.ru>
2025-03-13 20:16:54 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,60 +1,60 @@
-// Copyright 2022 NNTU-CS
 #include <cstdint>
 #include "alg.h"
 
-
 bool checkPrime(uint64_t value) {
-   if (value < 2) return false;
-    for (uint64_t i = 2; i * i <= value; ++i) {
-        if (value % i == 0) return false;
+  if (value < 2) {
+    return false;
+  }
+  for (uint64_t i = 2; i * i <= value; ++i) {
+    if (value % i == 0) {
+      return false;
     }
-    return true;
-  
+  }
+  return true;
 }
 
 uint64_t nPrime(uint64_t n) {
   uint64_t count = 0;
-    uint64_t num = 2;
-    while (count < n) {
-        if (checkPrime(num)) {
-            ++count;
-        }
-        ++num;
+  uint64_t num = 2;
+  while (count < n) {
+    if (checkPrime(num)) {
+      ++count;
     }
-    return num - 1;
+    ++num;
+  }
+  return num - 1;
 }
 
 uint64_t nextPrime(uint64_t value) {
   uint64_t num = value + 1;
-    while (true) {
-        if (checkPrime(num)) {
-            return num;
-        }
-        ++num;
+  while (true) {
+    if (checkPrime(num)) {
+      return num;
     }
+    ++num;
+  }
 }
-
 
 uint64_t sumPrime(uint64_t hbound) {
   uint64_t sum = 0;
-    for (uint64_t i = 2; i < hbound; ++i) {
-        if (checkPrime(i)) {
-            sum += i;
-        }
+  for (uint64_t i = 2; i < hbound; ++i) {
+    if (checkPrime(i)) {
+      sum += i;
     }
-    return sum;
+  }
+  return sum;
 }
 
 uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
- uint64_t count = 0;
-    uint64_t prevPrime = 0;
-    for (uint64_t i = lbound; i < hbound; ++i) {
-        if (checkPrime(i)) {
-            if (i - prevPrime == 2) {
-                ++count;
-            }
-            prevPrime = i;
-        }
+  uint64_t count = 0;
+  uint64_t prevPrime = 0;
+  for (uint64_t i = lbound; i < hbound; ++i) {
+    if (checkPrime(i)) {
+      if (i - prevPrime == 2) {
+        ++count;
+      }
+      prevPrime = i;
     }
-    return count;
+  }
+  return count;
 }
