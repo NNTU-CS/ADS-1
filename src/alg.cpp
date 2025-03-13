090450e575b70b3935d5c8a0fc50fe472548a549
antihackss <vladmakov0859@gmail.com>
2025-03-13 21:55:56 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -24,6 +24,14 @@ uint64_t nPrime(uint64_t n) {
 }
 
 uint64_t nextPrime(uint64_t value) {
+  uint64_t next = value + 1;
+  while (!checkPrime(next)) {
+    next++;
+  }
+  return next;
+}
+
+uint64_t sumPrime(uint64_t hbound) {
   uint64_t sum = 0;
   for (uint64_t i = 2; i < hbound; i++) {
     if (checkPrime(i)) {
@@ -31,14 +39,6 @@ uint64_t nextPrime(uint64_t value) {
     }
   }
   return sum;
-}
-
-uint64_t sumPrime(uint64_t hbound) {
-  uint64_t next = value + 1;
-  while (!checkPrime(next)) {
-    next++;
-  }
-  return next;
 }
 
 uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
