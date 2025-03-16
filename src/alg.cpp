824e8adf5c996cb297fa04dbab06780cfe6213ad
annChekh <anua.chekhonadskikh@mail.ru>
2025-03-16 16:48:52 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -45,16 +45,12 @@ uint64_t sumPrime(uint64_t hbound) {
 }
 
 uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
-  uint64_t countPair = 0, prePrime = 0;
-  if (lbound < 2) {
-    lbound = 2;
-  }
-  for (uint64_t i = lbound; i < hbound; i++) {
-    if (checkPrime(i)) {
-      if (i - prePrime == 2) {
+  uint64_t countPair = 0;
+  for (uint64_t i = lbound; i < hbound - 2; i++) {
+    if (checkPrime(i) && checkPrime(i + 2)) {
+      if (i + 2 < hbound) {
         countPair++;
       }
-      prePrime = i;
     }
   }
   return countPair;
