416c6b64405d2df5cb9a175bf010f27a4424e968
ossicc <ossic8881@gmail.com>
2025-03-20 19:22:02 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,16 +3,16 @@
 
 
 bool checkPrime(uint64_t value) {
-  for (int i = 2; i < value; i++) {
-	if (value % i == 0) {
-		return false;
+	for (int i = 2; i < value; i++) {
+		if (value % i == 0) {
+			return false;
+		}
 	}
-}
-return true;
+	return true;
 }
 
-uint64_t nPrime(uint64_t n) {
-  	if (n == 1) {
+uint64_t nPrime(int n) {
+	if (n == 1) {
 		return 2;
 	}
 	else {
@@ -37,57 +37,59 @@ uint64_t nPrime(uint64_t n) {
 }
 
 uint64_t nextPrime(uint64_t value) {
-  int k;
-for (int i = value + 1; i > 0; i++) {
-	k = 0;
-	for (int j = 2; j < i; j++) {
-		if (i % j == 0) {
-			k++;
-		}
-	}
-	if (k == 0) {
-		return i;
-	}
-}
-}
-
-uint64_t sumPrime(uint64_t hbound) {
-  int sum = 0, k;
-for (int i = 2; i < hbound; i++) {
-	k = 0;
-	for (int j = 2; j < i; j++) {
-		if (i % j == 0) {
-			k++;
-		}
-	}
-	if (k == 0) {
-		sum += i;
-	}
-}
-return sum;
-}
-
-uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
-  int k, sch = 0;
-for (int i = lbound; i < hbound; i ++) {
-	k = 0;
-	for (int j = 2; j < i; j++) {
-		if (i % j == 0) {
-			k++;
-		}
-	}
-	int q = i + 2;
-	if (k == 0) {
+	int k;
+	for (int i = value + 1; i > 0; i++) {
 		k = 0;
-		for (int i = 2; i < q; i++) {
-			if (q % i == 0) {
+		for (int j = 2; j < i; j++) {
+			if (i % j == 0) {
 				k++;
 			}
 		}
 		if (k == 0) {
-			sch++;
+			return i;
 		}
 	}
+}
+
+uint64_t sumPrime(uint64_t hbound) {
+	int sum = 0, k;
+	for (int i = 2; i < hbound; i++) {
+		k = 0;
+		for (int j = 2; j < i; j++) {
+			if (i % j == 0) {
+				k++;
+			}
+		}
+		if (k == 0) {
+			sum += i;
+		}
+	}
+	return sum;
+}
+
+uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
+	int k, sch = 0;
+	for (int i = lbound; i < hbound-2; i ++) {
+		k = 0;
+		for (int j = 2; j < i; j++) {
+			if (i % j == 0) {
+				k++;
+			}
+		}
+		int q = i + 2;
+		if (k == 0) {
+			k = 0;
+			for (int i = 2; i < q; i++) {
+				if (q % i == 0) {
+					k++;
+				}
+			}
+			if (k == 0) {
+				sch++;
+			}
+		}
+	}
+	return sch;
 }
 return sch;
 }
