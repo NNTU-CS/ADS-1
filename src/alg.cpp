bc74e849d7ff480f0b166ebfc2bb3c45ce8f50d9
ossicc <ossic8881@gmail.com>
2025-03-22 11:15:05 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -30,9 +30,8 @@ uint64_t nPrime(uint64_t n) {
 }
 
 uint64_t nextPrime(uint64_t value) {
-	int k;
 	for (int i = value + 1; i > 0; i++) {
-		k = 0;
+		int k = 0;
 		for (int j = 2; j < i; j++) {
 			if (i % j == 0) {
 				k++;
@@ -62,26 +61,26 @@ uint64_t sumPrime(uint64_t hbound) {
 }
 
 uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
-	int k, sch = 0;
-	for (int i = lbound; i < hbound-2; i ++) {
-		k = 0;
-		for (int j = 2; j < i; j++) {
-			if (i % j == 0) {
-				k++;
-			}
-		}
-		int q = i + 2;
-		if (k == 0) {
-			k = 0;
-			for (int i = 2; i < q; i++) {
-				if (q % i == 0) {
-					k++;
-				}
-			}
-			if (k == 0) {
-				sch++;
-			}
-		}
-	}
-	return sch;
-};
+  int k, sch = 0;
+  for (lbound; lbound < hbound - 2; lbound++) {
+    k = 0;
+    for (int j = 2; j < lbound; j++) {
+      if (lbound % j == 0) {
+        k++;
+      }
+    }
+    int q = lbound + 2;
+    if (k == 0) {
+      k = 0;
+      for (int i = 2; i < q; i++) {
+        if (q % i == 0) {
+          k++;
+        }
+      }
+      if (k == 0) {
+        sch++;
+      }
+    }
+  }
+  return sch;
+}
