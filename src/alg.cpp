cd2f5ac78849507200c332f4ba64a8a7b9eebabf
ossicc <ossic8881@gmail.com>
2025-03-22 13:14:14 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -45,7 +45,7 @@ uint64_t nextPrime(uint64_t value) {
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-  int sum = 0, k=1;
+  int sum = 0;
   for (int i = 2; i < hbound; i++) {
     if (checkPrime(i) == true) {
       sum += i;
