51bc571802b236ba513c0ce97d38df3a6e777d00
kre1m <aksenovnikita2006@gmail.com>
2025-03-26 21:48:56 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,12 +5,12 @@
 using namespace std;
 
 bool checkPrime(uint64_t value) {
-	if (value <= 1) return false;
-	if  (value <= 3) return true;
-	if  (value % 2 == 0 || value % 3 == 0) return false;
+	 if (value <= 1) return false;
+	 if  (value <= 3) return true;
+	 if  (value % 2 == 0 || value % 3 == 0) return false;
 	for  (uint64_t i = 5; i * i <= value; i += 6) {
-		if (value % i == 0 || value % (i + 2) == 0) return false;
-	}
+		 if (value % i == 0 || value % (i + 2) == 0) return false;
+	 }
 	return true;
 }
 
