be1ab809f12eeb32a51c127a443c61bb2f632c34
EsheTotPri <eshetotpridurok@gmail.com>
2025-03-28 19:25:57 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,7 +4,7 @@
 
 
 bool checkPrime(uint64_t value) {
-    for (int i = 2; i < (value/2)+1; i++){
+    for (int i = 2; i < (value/2)+1; i++) {
         if (value % i == 0)
             return false;
     }
@@ -35,15 +35,15 @@ uint64_t nPrime(uint64_t n) {
 
 uint64_t nextPrime(uint64_t value) {
     uint64_t n = value + 1;
-    for (uint64_t i = n; ; i++){
+    for (uint64_t i = n; ; i++) {
         bool flag = true;
-        for (int j = 2; j <= (i / 2)+1; j++){
-            if(i % j == 0){
+        for (int j = 2; j <= (i / 2)+1; j++) {
+            if(i % j == 0) {
                 flag = false;
                 break;
             }
         }
-        if (flag == true){
+        if (flag == true) {
             return i;
         }
     }
@@ -52,15 +52,15 @@ uint64_t nextPrime(uint64_t value) {
 
 uint64_t sumPrime(uint64_t hbound) {
     uint64_t sum = 0;
-    for (uint64_t i = 2; i < hbound; i++){
+    for (uint64_t i = 2; i < hbound; i++) {
         bool flag = true;
-        for (uint64_t j = 2; j <= i / 2; j++){
-            if (i % j == 0){
+        for (uint64_t j = 2; j <= i / 2; j++) {
+            if (i % j == 0) {
                 flag = false;
                 break;
             }
         }
-        if (flag == true){
+        if (flag == true) {
             sum+=i;
         }
     }
@@ -69,15 +69,15 @@ uint64_t sumPrime(uint64_t hbound) {
 
 uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
     int c = 0;
-    for (uint64_t i = lbound; i+2 < hbound; i++){
+    for (uint64_t i = lbound; i+2 < hbound; i++) {
         bool flag = true;
-        for (int j = 2; j <= (i / 2)+1; j++){
-            if (i % j == 0 || (i + 2) % j == 0){
+        for (int j = 2; j <= (i / 2)+1; j++) {
+            if (i % j == 0 || (i + 2) % j == 0) {
                 flag = false;
                 break;
             }
         }
-        if (flag == true){
+        if (flag == true) {
             c++;
         }
     }
