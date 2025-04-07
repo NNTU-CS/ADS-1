8f2b134f3df0bfda70fc5cb2ca378e36b1b56064
egorisan <epodcherednichenko@gmail.com>
2025-04-08 00:59:10 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,27 +3,43 @@
 #include "alg.h"
 
 
-bool checkPrime(uint64_t value) {
-  // вставьте код функции
-  return true;
-}
+bool checkPrime(uint64_t value){
+  uint64_t koef = 0;
+  for (int el = 1; el <= sqrt(value); el++){
+    if (value % el == 0) k += 1;
+  }
+  if (k == 1) return true;
+  else{return false;}}
 
 uint64_t nPrime(uint64_t n) {
-  // вставьте код функции
-  return 2;
+  uint64_t el = 0, a = 2;
+  while (el < n) {
+    if (checkPrime(a)) el += 1;
+    a += 1;
+  }
+  return a - 1;
 }
 
 uint64_t nextPrime(uint64_t value) {
-  // вставьте код функции
-  return 2;
+  value += 1;
+  while (!checkPrime(value)){value += 1;}
+  return value;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-  // вставьте код функции
-  return 2;
+  uint64_t el = 2, summa = 0;
+  while (el < hbound) {
+    if (checkPrime(el)) summa += el;
+    el += 1;
+  }
+  return summa;
 }
 
 uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
-  // вставьте код функции
-  return 1;
+  uint64_t k = 0, a = lbound;
+  while (a + 2 < hbound) {
+    if (checkPrime(a) && checkPrime(a + 2)) k += 1;
+    a += 1;
+  }
+  return k;
 }
