5fd352623bbf68aa9920830c455a53c13bba830a
kirill <prettyplugonline@gmail.com>
2025-04-10 15:23:08 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,6 @@
-// Copyright 2022 NNTU-CS
 #include <cstdint>
 #include "alg.h"
+// Copyright 2022 NNTU-CS
 
 bool checkPrime(uint64_t value) {
   if (value <= 1) return false;
@@ -14,54 +14,54 @@ bool checkPrime(uint64_t value) {
 
 uint64_t nPrime(uint64_t n) {
   if (n == 1) {
-        return 2;
+    return 2;
     }
   uint64_t count = 1;
   uint64_t number = 3;
   while (count < n) {
-      if (checkPrime(number)) {
-          count++;
-      }
-      if (count == n) {
-          return number;
-      }
-      number += 2;
+    if (checkPrime(number)) {
+      count++;
+    }
+    if (count == n) {
+        return number;
+    }
+    number += 2;
   }
   return 0;
 }
 
 uint64_t nextPrime(uint64_t value) {
   if (value < 2) {
-      return 2;
+    return 2;
   }
   uint64_t next = value + 1;
   while (true) {
-      if (checkPrime(next)) {
-          return next;
-      }
-      next++;
+    if (checkPrime(next)) {
+      return next;
+    }
+    next++;
   }
 }
 
 uint64_t sumPrime(uint64_t hbound) {
   uint64_t sum = 0;
   for (uint64_t i = 2; i < hbound; ++i) {
-      if (checkPrime(i)) {
-          sum += i;
-      }
+    if (checkPrime(i)) {
+      sum += i;
+    }
   }
   return sum;
 }
 
 uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
   if (lbound >= hbound - 1) {
-      return 0;
+    return 0;
   }
   uint64_t count = 0;
   for (uint64_t i = lbound; i <= hbound - 2; ++i) {
-      if (checkPrime(i) && checkPrime(i + 2)) {
-          count++;
-      }
+    if (checkPrime(i) && checkPrime(i + 2)) {
+      count++;
+    }
   }
   return count;
 }
