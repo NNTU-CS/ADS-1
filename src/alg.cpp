03d3608ae9322803413a4e89b6bf13068bfe423a
antihackss <vladmakov0859@gmail.com>
2025-04-16 15:31:35 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,3 +1,4 @@
+// Copyright 2022 NNTU-CS
 #include <cstdint>
 #include <cmath>
 
@@ -5,7 +6,7 @@ bool checkPrime(uint64_t value) {
   if (value < 2) return false;
   if (value == 2) return true;
   if (value % 2 == 0) return false;
-  
+
   for (uint64_t i = 3; i * i <= value; i += 2) {
     if (value % i == 0) return false;
   }
@@ -16,7 +17,7 @@ uint64_t nPrime(uint64_t n) {
   if (n == 0) return 0;
   uint64_t count = 0;
   uint64_t num = 1;
-  
+
   while (count < n) {
     num++;
     if (checkPrime(num)) {
@@ -28,12 +29,12 @@ uint64_t nPrime(uint64_t n) {
 
 uint64_t nextPrime(uint64_t value) {
   if (value < 2) return 2;
-  
+
   uint64_t candidate = value + 1;
   if (candidate % 2 == 0) {
     candidate++;
   }
-  
+
   while (true) {
     if (checkPrime(candidate)) {
       return candidate;
@@ -55,7 +56,7 @@ uint64_t sumPrime(uint64_t hbound) {
 uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
   uint64_t count = 0;
   uint64_t last_prime = 0;
-  
+
   if (lbound <= 2) {
     if (hbound > 3 && checkPrime(3)) {
       last_prime = 3;
@@ -64,7 +65,7 @@ uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
   } else if (lbound % 2 == 0) {
     lbound++;
   }
-  
+
   for (uint64_t i = lbound; i < hbound; i += 2) {
     if (checkPrime(i)) {
       if (i - last_prime == 2) {
