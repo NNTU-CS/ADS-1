af6c8d9df2cdba04bee5fd2ba6c15ee15d969486
Darwitle <margonesnanowa@gmail.com>
2025-06-06 23:19:30 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,5 +2,10 @@
 #include "alg.h"
 
 int main() {
+    std::cout << "checkPrime(7): " << checkPrime(7) << "\n";
+    std::cout << "nPrime(5): " << nPrime(5) << "\n";
+    std::cout << "nextPrime(10): " << nextPrime(10) << "\n";
+    std::cout << "sumPrime(10): " << sumPrime(10) << "\n";
+    std::cout << "twinPrimes(2, 20): " << twinPrimes(2, 20) << "\n";
     return 0;
 }
