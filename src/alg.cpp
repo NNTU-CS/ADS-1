7ae1862da6b59dca3468db2cb22d3263e6498a9c
Darwitle <margonesnanowa@gmail.com>
2025-06-06 23:24:05 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,7 +2,7 @@
 #include <cstdint>
 #include "alg.h"
 
-
+// cppcheck-suppress unusedFunction
 bool checkPrime(uint64_t value) {
   if (value < 2) return false;
   if (value == 2 || value == 3) return true;
@@ -14,6 +14,7 @@ bool checkPrime(uint64_t value) {
   return true;
 }
 
+// cppcheck-suppress unusedFunction
 uint64_t nPrime(uint64_t n) {
   if (n == 0) return 0;
   uint64_t count = 0;
@@ -27,6 +28,7 @@ uint64_t nPrime(uint64_t n) {
   }
 }
 
+// cppcheck-suppress unusedFunction
 uint64_t nextPrime(uint64_t value) {
   value++;
   while (true) {
@@ -35,6 +37,7 @@ uint64_t nextPrime(uint64_t value) {
   }
 }
 
+// cppcheck-suppress unusedFunction
 uint64_t sumPrime(uint64_t hbound) {
   uint64_t sum = 0;
   for (uint64_t i = 2; i < hbound; ++i) {
@@ -43,6 +46,7 @@ uint64_t sumPrime(uint64_t hbound) {
   return sum;
 }
 
+// cppcheck-suppress unusedFunction
 uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
   uint64_t count = 0;
   for (uint64_t i = lbound; i + 2 < hbound; ++i) {
