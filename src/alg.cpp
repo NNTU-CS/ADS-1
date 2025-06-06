ac525701cd296a6dbd2674af48fdb6d189dfd50d
Darwitle <margonesnanowa@gmail.com>
2025-06-06 22:29:32 +0300

    235451
    314523342

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,54 +1,56 @@
+// Copyright 2025 Darwitle
+
 #include <cstdint>
 #include <cmath>
 
 bool checkPrime(uint64_t value) {
-  if (value<2) return false;
-  if (value==2 || value==3) return true;
-  if (value%2 == 0 || value%3 == 0) return false;
+    if (value < 2) return false;
+    if (value == 2 || value == 3) return true;
+    if (value % 2 == 0 || value % 3 == 0) return false;
 
-    for (uint64_t i=5; i * i<=value; i+=6) {
-    if (value%i==0 || value%(i+2)==0) 
-    return false;
-  }
-  return true;
+    for (uint64_t i = 5; i * i <= value; i += 6) {
+        if (value % i == 0 || value % (i + 2) == 0)
+            return false;
+    }
+    return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-  if (n==0) return 0;
-  uint64_t count=0;
-  uint64_t value=2;
+    if (n == 0) return 0;
+    uint64_t count = 0;
+    uint64_t value = 2;
     while (true) {
         if (checkPrime(value)) {
-      count++;
-      if (count==n)
-      return value;
+            count++;
+            if (count == n)
+                return value;
+        }
+        value++;
     }
-    value++;
-  }
 }
 
 uint64_t nextPrime(uint64_t value) {
-  value++;
-    while (true) {
-    if (checkPrime(value)) return value;
     value++;
-  }
+    while (true) {
+        if (checkPrime(value)) return value;
+        value++;
+    }
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-  uint64_t sum = 0;
-    for (uint64_t i=2; i<hbound; ++i) {
-    if (checkPrime(i)) sum+=i;
-  }
-  return sum;
+    uint64_t sum = 0;
+    for (uint64_t i = 2; i < hbound; ++i) {
+        if (checkPrime(i)) sum += i;
+    }
+    return sum;
 }
 
 uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
-  uint64_t count=0;
-    for (uint64_t i=lbound; i+2< hbound; ++i) {
-        if (checkPrime(i) && checkPrime(i+2)) {
-      count++;
+    uint64_t count = 0;
+    for (uint64_t i = lbound; i + 2 < hbound; ++i) {
+        if (checkPrime(i) && checkPrime(i + 2)) {
+            count++;
+        }
     }
-  }
-  return count;
+    return count;
 }
