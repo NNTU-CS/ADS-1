7d50b410abe9e5a3d632480246a26a3c44e0fbde
MariyaKorenkova <maryakoren@yandex.ru>
2025-06-08 22:23:22 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -33,20 +33,19 @@ uint64_t nextPrime(uint64_t value) {
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-  uint64_t first = 2;
   uint64_t sum = 0;
-  if (hbound <= 2) return 0;
-  while (first < hbound) {
-    sum += first;
-    first = nextPrime(first);
+  for (uint64_t i = 2; i < hbound; ++i) {
+    if (checkPrime(i)) sum += i;
   }
   return sum;
 }
 
 uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
   uint64_t count = 0;
-  for (int i = lbound; i < hbound; i++) {
-    if ((checkPrime(i) == true) && (checkPrime(i+2) == true)) count++;
+  for (uint64_t i = lbound; i + 2 < hbound; ++i) {
+    if (checkPrime(i) && checkPrime(i + 2)) {
+      count++;
+    }
   }
   return count;
 }
